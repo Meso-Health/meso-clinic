1d47b50f2ad0e0cdc5ef892eabbfca7e0be04a4d
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2016-12-26 11:08:54 -0500

    Implement simple search functionality
    [#136534967]

--- app/src/main/java/org/watsi/uhp/ReceptionActivity.java
+++ app/src/main/java/org/watsi/uhp/ReceptionActivity.java
@@ -1,13 +1,61 @@
 package org.watsi.uhp;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
-public class ReceptionActivity extends Activity {
+public class ReceptionActivity extends Activity implements SearchView.OnQueryTextListener {
+
+    private SearchView searchView;
+    private MenuItem searchMenuItem;
+    private ArrayAdapter<String> listAdapter;
+    private FilterableAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reception);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+
+        adapter = new FilterableAdapter(this);
+
+        ListView lv = (ListView) findViewById(R.id.list_view);
+        lv.setAdapter(adapter);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchMenuItem = menu.findItem(R.id.search);
+        searchView = (SearchView) searchMenuItem.getActionView();
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setOnQueryTextListener(this);
+
+        adapter.getFilter().filter(null);
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        adapter.getFilter().filter(newText);
+
+        return true;
     }
 }
