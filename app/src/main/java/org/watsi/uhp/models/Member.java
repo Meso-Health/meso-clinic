f4cad3f143c8cdafd0089b86a9a5d290b8f01d41
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2017-01-04 19:06:43 -0800

    Add additional fields to Member model
    [#136534993]

--- app/src/main/java/org/watsi/uhp/models/Member.java
+++ app/src/main/java/org/watsi/uhp/models/Member.java
@@ -3,6 +3,8 @@ package org.watsi.uhp.models;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
+import java.util.Date;
+
 @DatabaseTable(tableName = Member.TABLE_NAME_MEMBERS)
 public class Member {
 
@@ -10,12 +12,24 @@ public class Member {
 
     public static final String FIELD_NAME_ID = "id";
     public static final String FIELD_NAME_NAME = "name";
+    public static final String FIELD_NAME_BIRTHDATE = "birthdate";
+    public static final String FIELD_NAME_GENDER = "gender";
+    public static final String FIELD_NAME_PHONE_NUMBER = "phone_number";
 
     @DatabaseField(columnName = FIELD_NAME_ID, generatedId = true)
     private int mId;
 
     @DatabaseField(columnName = FIELD_NAME_NAME)
     private String mName;
+
+    @DatabaseField(columnName = FIELD_NAME_BIRTHDATE)
+    private Date mBirthdate;
+
+    @DatabaseField(columnName = FIELD_NAME_GENDER)
+    private GenderEnum mGender;
+
+    @DatabaseField(columnName = FIELD_NAME_PHONE_NUMBER)
+    private String mPhoneNumber;
 
     public Member() {
         // empty constructor necessary for ORM
@@ -36,4 +50,30 @@ public class Member {
     public int getId() {
         return this.mId;
     }
+
+    public Date getBirthdate() {
+        return mBirthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.mBirthdate = birthdate;
+    }
+
+    public GenderEnum getGender() {
+        return mGender;
+    }
+
+    public void setGender(GenderEnum gender) {
+        this.mGender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return mPhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.mPhoneNumber = phoneNumber;
+    }
+
+    private enum GenderEnum { M, F }
 }
