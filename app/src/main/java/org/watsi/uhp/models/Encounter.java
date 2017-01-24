937a6ad07c2acae90983d73fd447e0adf2d0a44f
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2017-01-24 13:08:22 +0300

    Add IdMethod enum to Encounter
    This field will store what method was used to identify the patient
    when recording an encounter

--- app/src/main/java/org/watsi/uhp/models/Encounter.java
+++ app/src/main/java/org/watsi/uhp/models/Encounter.java
@@ -13,15 +13,21 @@ public class Encounter {
     public static final String FIELD_NAME_ID = "id";
     public static final String FIELD_NAME_DATE = "date";
     public static final String FIELD_NAME_MEMBER_ID = "member_id";
+    public static final String FIELD_NAME_ID_METHOD = "id_method";
+
+    public enum IdMethodEnum { SEARCH, BARCODE, FINGERPRINT, RECENT }
 
     @DatabaseField(columnName = FIELD_NAME_ID, generatedId = true)
     private int mId;
 
-    @DatabaseField(columnName = FIELD_NAME_DATE)
+    @DatabaseField(columnName = FIELD_NAME_DATE, canBeNull = false)
     private Date mDate;
 
     @DatabaseField(columnName = FIELD_NAME_MEMBER_ID, foreign = true, canBeNull = false)
     private Member mMember;
+
+    @DatabaseField(columnName = FIELD_NAME_ID_METHOD, canBeNull = false)
+    private IdMethodEnum mIdMethod;
 
     public Encounter() {
         // empty constructor necessary for ORM
@@ -45,5 +51,13 @@ public class Encounter {
 
     public Member getMember() {
         return mMember;
+    }
+
+    public IdMethodEnum getIdMethod() {
+        return mIdMethod;
+    }
+
+    public void setIdMethod(IdMethodEnum idMethod) {
+        this.mIdMethod = idMethod;
     }
 }
