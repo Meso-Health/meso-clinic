500a11db732057150237f1f024e2913a920e8145
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2017-02-14 15:18:31 -0800

    Get Member unit test passing
    Included the PowerMock library to handle stubbing static methods to
    not have to access the DB in unit tests. Had to downgrade the
    Mockito library to version 1.+ because version 2 was not compatible
    with PowerMock.

--- app/src/test/java/org/watsi/uhp/models/MemberTest.java
+++ app/src/test/java/org/watsi/uhp/models/MemberTest.java
@@ -2,21 +2,41 @@ package org.watsi.uhp.models;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.watsi.uhp.database.EncounterDao;
+
+import java.util.Arrays;
+import java.util.Calendar;
 
 import static junit.framework.Assert.assertEquals;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(EncounterDao.class)
 public class MemberTest {
 
-    Member member;
+    private Member member;
 
     @Before
     public void setup() {
+        mockStatic(EncounterDao.class);
         member = new Member();
     }
 
     @Test
-    public void getName_getsTheName() throws Exception {
-        member.setName("Foo");
-        assertEquals(member.getName(), "Foo");
+    public void getLastEncounter() throws Exception {
+        Calendar cal = Calendar.getInstance();
+        Encounter e1 = new Encounter();
+        e1.setDate(cal.getTime());
+        Encounter e2 = new Encounter();
+        cal.add(Calendar.DAY_OF_MONTH, -5);
+        e2.setDate(cal.getTime());
+
+        Mockito.when(EncounterDao.find(Mockito.anyMap())).thenReturn(Arrays.asList(new Encounter[]{e2, e1}));
+
+        assertEquals(member.getLastEncounter(), e1);
     }
 }
