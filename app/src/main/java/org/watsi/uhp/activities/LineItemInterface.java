08f6c7199eede6a3fe341e6b2e6325c66f433824
katrinacrisostomo <katrina@watsi.org>
2017-02-20 00:27:55 +0300

    Remove redundancy from LineItemInterface methods
    The public was redundant

--- app/src/main/java/org/watsi/uhp/activities/LineItemInterface.java
+++ app/src/main/java/org/watsi/uhp/activities/LineItemInterface.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface LineItemInterface {
 
-    public void setCurrentLineItems();
+    void setCurrentLineItems(List<LineItem> lineItems);
 
-    public List<LineItem> getCurrentLineItems();
+    List<LineItem> getCurrentLineItems();
 }
