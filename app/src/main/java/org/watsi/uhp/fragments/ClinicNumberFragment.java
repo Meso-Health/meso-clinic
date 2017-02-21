0451e1c84957bf9d8901b358ba08438430ee13f1
katrinacrisostomo <katrina@watsi.org>
2017-02-21 09:45:11 +0300

    Setup Clinic Number Fragment

---
+++ app/src/main/java/org/watsi/uhp/fragments/ClinicNumberFragment.java
@@ -0,0 +1,33 @@
+package org.watsi.uhp.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import org.watsi.uhp.R;
+import org.watsi.uhp.models.Encounter;
+
+public class ClinicNumberFragment extends Fragment {
+    private Spinner numberTypeSpinner;
+    private EditText numberField;
+    private Button submitNumberButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
+        getActivity().setTitle(R.string.clinic_number_fragment_label);
+
+        View view = inflater.inflate(R.layout.fragment_clinic_number, container, false);
+
+        numberTypeSpinner = (Spinner) view.findViewById(R.id.clinic_number_type_dropdown);
+        numberField = (EditText) view.findViewById(R.id.clinic_number_field);
+        submitNumberButton = (Button) view.findViewById(R.id.clinic_number_continue_button);
+
+        return view;
+    }
+
+}
