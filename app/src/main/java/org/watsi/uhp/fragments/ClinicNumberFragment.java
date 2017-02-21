3b62ccf929c4df7626315d011603c25e7ccd2097
katrinacrisostomo <katrina@watsi.org>
2017-02-21 11:13:39 +0300

    Set number type spinner in clinic number frag

--- app/src/main/java/org/watsi/uhp/fragments/ClinicNumberFragment.java
+++ app/src/main/java/org/watsi/uhp/fragments/ClinicNumberFragment.java
@@ -5,12 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import org.watsi.uhp.R;
 import org.watsi.uhp.models.Encounter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class ClinicNumberFragment extends Fragment {
     private Spinner numberTypeSpinner;
@@ -27,7 +32,21 @@ public class ClinicNumberFragment extends Fragment {
         numberField = (EditText) view.findViewById(R.id.clinic_number_field);
         submitNumberButton = (Button) view.findViewById(R.id.clinic_number_continue_button);
 
+        setNumberTypeSpinner();
         return view;
+    }
+
+    private void setNumberTypeSpinner() {
+        ArrayList<Object> numberTypes = new ArrayList<>();
+        numberTypes.addAll(Arrays.asList(Encounter.ClinicNumberTypeEnum.values()));
+
+        final ArrayAdapter numberTypeAdapter = new ArrayAdapter<>(
+                getContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                numberTypes
+        );
+
+        numberTypeSpinner.setAdapter(numberTypeAdapter);
     }
 
 }
