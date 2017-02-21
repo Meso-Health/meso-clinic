1745d209b0d2e247dd0d97446cdc26b3850ad4f8
Byron Zhang <byron.yi.zhang@gmail.com>
2017-02-21 15:35:06 +0300

    Return List instead of Collection in getter method: + a collection will largely be useless.

--- app/src/main/java/org/watsi/uhp/models/Encounter.java
+++ app/src/main/java/org/watsi/uhp/models/Encounter.java
@@ -4,6 +4,7 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.field.ForeignCollectionField;
 import com.j256.ormlite.table.DatabaseTable;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
@@ -45,8 +46,8 @@ public class Encounter extends AbstractModel {
         this.mMember = member;
     }
 
-    public Collection<LineItem> getLineItems() {
-        return mLineItems;
+    public List<LineItem> getLineItems() {
+        return new ArrayList<>(mLineItems);
     }
 
     public void setLineItems(Collection<LineItem> lineItems) {
