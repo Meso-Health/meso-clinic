bf01ed24f1e4a6dd1860e0ecc2b13950df51c1bf
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2017-02-28 00:02:48 +0300

    Capture photos during enrollment completion
    [#140651807]

--- app/src/main/java/org/watsi/uhp/fragments/EnrollmentPhotosFragment.java
+++ app/src/main/java/org/watsi/uhp/fragments/EnrollmentPhotosFragment.java
@@ -1,16 +1,31 @@
 package org.watsi.uhp.fragments;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.rollbar.android.Rollbar;
 
 import org.watsi.uhp.R;
 import org.watsi.uhp.managers.NavigationManager;
+import org.watsi.uhp.models.Member;
 
 public class EnrollmentPhotosFragment extends Fragment {
+
+    static final int TAKE_MEMBER_PHOTO_INTENT = 1;
+    static final int TAKE_ID_PHOTO_INTENT = 2;
+
+    private ImageView mMemberPhotoImageView;
+    private ImageView mIdPhotoImageView;
+    private Member mMember;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
@@ -24,6 +39,53 @@ public class EnrollmentPhotosFragment extends Fragment {
                 new NavigationManager(getActivity()).setEnrollmentFingerprintFragment();
             }
         });
+
+        Button captureMemberPhotoBtn =
+                (Button) view.findViewById(R.id.enrollment_photos_member_photo_button);
+        captureMemberPhotoBtn.setOnClickListener(
+                new CapturePhotoClickListener(TAKE_MEMBER_PHOTO_INTENT, this));
+
+        Button captureIdPhotoBtn =
+                (Button) view.findViewById(R.id.enrollment_photos_id_photo_button);
+        captureIdPhotoBtn.setOnClickListener(
+                new CapturePhotoClickListener(TAKE_ID_PHOTO_INTENT, this));
+
+        mMemberPhotoImageView = (ImageView) view.findViewById(R.id.member_photo);
+        mIdPhotoImageView = (ImageView) view.findViewById(R.id.id_photo);
+
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == TAKE_MEMBER_PHOTO_INTENT) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            mMemberPhotoImageView.setImageBitmap(photo);
+        } else if (requestCode == TAKE_ID_PHOTO_INTENT) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            mIdPhotoImageView.setImageBitmap(photo);
+        } else {
+            Rollbar.reportMessage("Unknown activity result in EnrollmentPhotosFragment: " + requestCode);
+        }
+    }
+
+    private static class CapturePhotoClickListener implements View.OnClickListener {
+
+        private int mRequestCode;
+        private Fragment mFragment;
+
+        public CapturePhotoClickListener(int intentRequestCode, Fragment fragment) {
+            this.mRequestCode = intentRequestCode;
+            this.mFragment = fragment;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent takeMemberPhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            PackageManager packageManager = mFragment.getActivity().getPackageManager();
+            if (takeMemberPhotoIntent.resolveActivity(packageManager) != null) {
+                mFragment.startActivityForResult(takeMemberPhotoIntent, mRequestCode);
+            }
+        }
     }
 }
