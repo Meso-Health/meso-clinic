b2ab985293ecfa03f9679743a5bb1d22008ee283
Byron Zhang <byron.yi.zhang@gmail.com>
2017-03-09 09:35:45 -0800

    Typo.
    [no tracker]

--- deploy_scripts/assembleSignedRelease.sh
+++ deploy_scripts/assembleSignedRelease.sh
@@ -3,7 +3,7 @@
 export VERSION_CODE=${CIRCLE_BUILD_NUM}
 export VERSION_NAME="${CURRENT_BUILD_MAJOR}.${CURRENT_BUILD_MINOR}.${CIRCLE_BUILD_NUM}"
 
-echo "These are the verisonCode and versionName that we are using to build the apk."
+echo "These are the versionCode and versionName that we are using to build the apk."
 echo ${VERSION_CODE}
 echo ${VERSION_NAME}
 
