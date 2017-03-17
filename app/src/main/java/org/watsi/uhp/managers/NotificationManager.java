c54d16edf02c66cc6f0de8ede332df889eefccfa
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2017-03-17 22:22:14 +0300

    Fix unit test
    [#141385847]

--- app/src/main/java/org/watsi/uhp/managers/NotificationManager.java
+++ app/src/main/java/org/watsi/uhp/managers/NotificationManager.java
@@ -7,17 +7,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class NotificationManager {
     public static void requestFailure(String description, Request request, Response response, Map<String,String> params) {
         params.put("Url", request.url().toString());
         params.put("Method", request.method());
-        params.put("Content-Type", request.body().contentType().toString());
-        try {
-            params.put("Content-Length", String.valueOf(request.body().contentLength()));
-        } catch (IOException e) {
-            Rollbar.reportException(e);
+        RequestBody body = request.body();
+        if (body != null) {
+            if (body.contentType() != null) params.put("Content-Type", request.body().contentType().toString());
+            try {
+                params.put("Content-Length", String.valueOf(request.body().contentLength()));
+            } catch (IOException e) {
+                Rollbar.reportException(e);
+            }
         }
         if (response != null) {
             params.put("X-Request-Id", response.header("X-Request-Id"));
