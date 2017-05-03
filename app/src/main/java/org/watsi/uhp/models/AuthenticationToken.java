ed47a5a28608f8d36515cf6d0c0ddeb92c0c1e18
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2017-05-03 16:59:38 -0700

    Remove unused code

--- app/src/main/java/org/watsi/uhp/models/AuthenticationToken.java
+++ app/src/main/java/org/watsi/uhp/models/AuthenticationToken.java
@@ -1,16 +1,8 @@
 package org.watsi.uhp.models;
 
 import com.google.gson.annotations.SerializedName;
-import com.rollbar.android.Rollbar;
-
-import org.watsi.uhp.managers.Clock;
-
-import java.text.ParseException;
-import java.util.Date;
 
 public class AuthenticationToken {
-
-    public static int REFRESH_TOKEN_THRESHOLD = 1000 * 60 * 60 * 24 * 7; // 7 days in milliseconds
 
     @SerializedName("token")
     private String token;
@@ -45,19 +37,5 @@ public class AuthenticationToken {
 
     public void setUser(User user) {
         this.user = user;
-    }
-
-    public long timeTillExpiration() {
-        try {
-            Date expiresAtDate = Clock.ISO_DATE_FORMAT.parse(getExpiresAt());
-            return expiresAtDate.getTime() - Clock.getCurrentTime().getTime();
-        } catch (ParseException e) {
-            Rollbar.reportException(e);
-            return -1;
-        }
-    }
-
-    public boolean shouldRefreshToken() {
-        return timeTillExpiration() <= REFRESH_TOKEN_THRESHOLD;
     }
 }
