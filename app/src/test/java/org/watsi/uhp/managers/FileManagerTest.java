744f4dabcd7540e2905d55656135fced144cc515
Michael Liang <liangmicha@gmail.com>
2017-05-09 12:18:20 -0700

    Remove some more unused imports
    [no tracker]

--- app/src/test/java/org/watsi/uhp/managers/FileManagerTest.java
+++ app/src/test/java/org/watsi/uhp/managers/FileManagerTest.java
@@ -15,7 +15,6 @@ import java.io.File;
 
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
-import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyMap;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
