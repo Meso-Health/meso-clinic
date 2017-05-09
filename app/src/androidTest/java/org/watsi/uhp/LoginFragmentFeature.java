d8ab1dc2ff065e64b0418d37d67e713a29d0fd1d
katrinacrisostomo <katrina@watsi.org>
2017-05-09 16:22:05 -0700

    Add RunWith line in login feature test
    [#140568545]

--- app/src/androidTest/java/org/watsi/uhp/LoginFragmentFeature.java
+++ app/src/androidTest/java/org/watsi/uhp/LoginFragmentFeature.java
@@ -1,10 +1,12 @@
 package org.watsi.uhp;
 
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.watsi.uhp.activities.MainActivity;
 import org.watsi.uhp.basetests.ActivityTest;
 import org.watsi.uhp.managers.NavigationManager;
@@ -20,6 +22,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
+@RunWith(AndroidJUnit4.class)
 public class LoginFragmentFeature extends ActivityTest {
 
     private static final String USERNAME = "klinik";
