88756e3341470d485bbadb3bea0ce53d799a4c7b
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2017-05-12 14:25:21 -0700

    First step in abstracting and simplifying syncing logic
    Moves most of the syncing related logic into SyncableModel so that it is
    consistent across all models. Simplifies the synced, dirty_fields and is_new
    fields by just using dirty_fields and having synced and is_new be implicitly
    derived from the dirty_fields - if dirty_fields is not empty, the model is
    considered unsynced, and if the id field is marked as dirty, the model is
    considered new.
    
    Also consolidates the logic around how sync requests are formed by moving
    all shared logic into SyncableModel and having abstract methods for the
    model-specific syncing logic.
    
    [#145314915]

--- app/src/main/java/org/watsi/uhp/models/SyncableModel.java
+++ app/src/main/java/org/watsi/uhp/models/SyncableModel.java
@@ -1,33 +1,62 @@
 package org.watsi.uhp.models;
 
+import android.content.Context;
+
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.google.gson.Gson;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.field.DatabaseField;
 
+import org.watsi.uhp.BuildConfig;
+import org.watsi.uhp.database.DatabaseHelper;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
+
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Response;
 
 /**
  * Abstract class for specifying any fields and behavior that all
  * models that need to be synced to the back-end should share
  */
-public abstract class SyncableModel extends AbstractModel {
+public abstract class SyncableModel<T extends SyncableModel<T>> extends AbstractModel {
 
+    public static final String FIELD_NAME_ID = "id";
     public static final String FIELD_NAME_TOKEN = "token";
-    public static final String FIELD_NAME_SYNCED = "synced";
     public static final String FIELD_NAME_DIRTY_FIELDS = "dirty_fields";
-    public static final String FIELD_NAME_IS_NEW = "is_new";
+
+    protected static final Set<String> SYNCABLE_DIFF_IGNORE_FIELDS = Sets.newHashSet(new String[]{
+            "mToken","mDirtyFields"});
+
+    @Expose
+    @SerializedName(FIELD_NAME_ID)
+    @DatabaseField(columnName = FIELD_NAME_ID, generatedId = true)
+    private UUID mId;
 
     @DatabaseField(columnName = FIELD_NAME_TOKEN)
     private String mToken;
 
-    @DatabaseField(columnName = FIELD_NAME_SYNCED, defaultValue = "false", canBeNull = false)
-    private boolean mSynced;
-
     @DatabaseField(columnName = FIELD_NAME_DIRTY_FIELDS, defaultValue = "[]", canBeNull = false)
     private String mDirtyFields = "[]";
 
-    @DatabaseField(columnName = FIELD_NAME_IS_NEW, canBeNull = false, defaultValue = "false")
-    private boolean mIsNew;
+    public UUID getId() {
+        return this.mId;
+    }
+
+    protected void setId(UUID id) {
+        this.mId = id;
+    }
 
     public String getToken() {
         return mToken;
@@ -37,35 +66,15 @@ public abstract class SyncableModel extends AbstractModel {
         this.mToken = token;
     }
 
-    public boolean isSynced() {
-        return mSynced;
-    }
-
-    public void setSynced() throws ValidationException {
-        if (isDirty()) {
-            String dirtyField = (String) getDirtyFields().toArray()[0];
-            throw new ValidationException(dirtyField, "Cannot mark dirty model as synced");
-        } else {
-            setToken(null);
-            setIsNew(false);
-            this.mSynced = true;
-        }
-    }
-
-    public void setUnsynced(String token) throws UnauthenticatedException {
-        if (token == null) {
-            throw new UnauthenticatedException();
-        } else {
-            setToken(token);
-            this.mSynced = false;
-        }
+    public boolean isSynced() throws SQLException {
+        return !isNew() && getDirtyFields().isEmpty();
     }
 
     private void setDirtyFields(Set<String> dirtyFields) {
         this.mDirtyFields = new Gson().toJson(dirtyFields);
     }
 
-    private Set<String> getDirtyFields() {
+    protected Set<String> getDirtyFields() {
         if (this.mDirtyFields == null) {
             return new HashSet<>();
         } else {
@@ -73,45 +82,111 @@ public abstract class SyncableModel extends AbstractModel {
         }
     }
 
-    public Boolean isNew() {
-        return mIsNew;
+    protected Dao<T,UUID> getDao() throws SQLException {
+        return (Dao<T, UUID>) DatabaseHelper.getHelper().getDao(this.getClass());
     }
 
-    public void setIsNew(Boolean isNew) {
-        this.mIsNew = isNew;
+    public Boolean isNew() throws SQLException {
+        return getId() == null || dirty(FIELD_NAME_ID) ||
+                (getId() != null && !getDao().idExists(getId()));
     }
 
-    public boolean dirty(String fieldName) {
+    boolean dirty(String fieldName) {
         return getDirtyFields().contains(fieldName);
     }
 
-    void addDirtyField(String field) {
-        Set<String> currentFields = getDirtyFields();
-        currentFields.add(field);
-        setDirtyFields(currentFields);
-    }
-
-    void removeDirtyField(String field) {
-        Set<String> currentFields = getDirtyFields();
-        currentFields.remove(field);
-        setDirtyFields(currentFields);
+    boolean isDirty() {
+        return !getDirtyFields().isEmpty();
     }
 
     void clearDirtyFields() {
         setDirtyFields(new HashSet<String>());
     }
 
-    public boolean isDirty() {
-        return !getDirtyFields().isEmpty();
-    }
-
     public String getTokenAuthHeaderString() {
         return "Token " + getToken();
     }
 
+    Set<String> changedFields() throws SQLException {
+        if (isNew()) {
+            return diffFields(null);
+        } else {
+            return diffFields(getDao().queryForId(getId()));
+        }
+    }
+
+    Set<String> diffFields(T refModel) {
+        Gson gson = new Gson();
+        Map<String, Object> selfMap = gson.fromJson(gson.toJson(this), Map.class);
+        Map<String, Object> refMap;
+        if (refModel == null) {
+            refMap = new HashMap<>();
+        } else {
+            refMap = gson.fromJson(gson.toJson(refModel), Map.class);
+        }
+        MapDifference<String, Object> diff = Maps.difference(selfMap, refMap);
+
+        Set<String> diffSet = new HashSet<>();
+        diffSet.addAll(diff.entriesDiffering().keySet());
+        diffSet.addAll(diff.entriesOnlyOnLeft().keySet());
+        diffSet.addAll(diff.entriesOnlyOnRight().keySet());
+        diffSet.removeAll(diffIgnoreFields());
+        return diffSet;
+    }
+
+    public void saveChanges(String token) throws SQLException, UnauthenticatedException {
+        if (token == null) {
+            throw new UnauthenticatedException();
+        }
+        setToken(token);
+        if (this.mId == null) this.mId = UUID.randomUUID();
+        setDirtyFields(changedFields());
+        getDao().createOrUpdate((T) this);
+        persistAssociations();
+    }
+
+    public void updateFromSync() throws SQLException {
+        handleUpdate();
+        clearDirtyFields();
+        getDao().createOrUpdate((T) this);
+    }
+
+    public Response<T> sync(Context context)
+            throws UnauthenticatedException, SyncException, SQLException, IOException {
+        if (getToken() == null) {
+            throw new UnauthenticatedException();
+        }
+        if (!isDirty()) {
+            throw new SyncException();
+        }
+        if (isNew()) {
+            return postApiCall(context, getToken(), BuildConfig.PROVIDER_ID, this).execute();
+        } else {
+            return patchApiCall(context, getToken(), getId(), patchRequestBody(context)).execute();
+        }
+    }
+
+    protected Set<String> diffIgnoreFields() {
+        return SYNCABLE_DIFF_IGNORE_FIELDS;
+    }
+
+    public abstract void handleUpdate();
+    protected abstract Map<String, RequestBody> patchRequestBody(Context context);
+    protected abstract Call<T> postApiCall(
+            Context context, String token, int providerId, SyncableModel<T> model);
+    protected abstract Call<T> patchApiCall(
+            Context context, String token, UUID id, Map<String, RequestBody> params);
+    protected abstract void persistAssociations();
+
+    private static class SyncException extends Exception {
+        SyncException() {
+            super("Model does not have any fields that need to be synced");
+        }
+    }
+
     public static class UnauthenticatedException extends Exception {
-        public UnauthenticatedException() {
-            super("User is not authenticated");
+        UnauthenticatedException() {
+            super("Current user is not authenticated");
         }
     }
 }
--- app/src/test/java/org/watsi/uhp/models/SyncableModelTest.java
+++ app/src/test/java/org/watsi/uhp/models/SyncableModelTest.java
@@ -1,101 +1,330 @@
 package org.watsi.uhp.models;
 
+import android.content.Context;
+
+import com.j256.ormlite.dao.Dao;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.watsi.uhp.BuildConfig;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Response;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 import static junit.framework.Assert.fail;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Response.class)
 public class SyncableModelTest {
 
+    @Mock
+    Context mockContext;
+    @Mock
+    Call<Member> mockCall;
+    @Mock
+    Map<String, RequestBody> mockParams;
+    @Mock
+    Response<Member> mockResponse;
+
     // SyncableModel is an abstract class so testing with an implementation
-    private Member member;
+    private Member member = new Member();
+    private Member memberSpy = spy(member);
+    private UUID id = UUID.randomUUID();
+    private String token = "foo";
+
+    @Mock
+    Dao<Member, UUID> mockDao;
 
     @Before
-    public void setup() {
+    public void setup() throws Exception {
         MockitoAnnotations.initMocks(this);
-        member = new Member();
+        doReturn(mockDao).when(memberSpy).getDao();
     }
 
     @Test
-    public void setUnsynced_nullToken_throwsUnauthenticatedException() throws Exception {
-        try {
-            member.setUnsynced(null);
-            fail("Should throw validation exception");
-        } catch (Exception e) {
-            assertEquals(e.getMessage(), "User is not authenticated");
-        }
+    public void isSynced_isNew_returnsFalse() throws Exception {
+        when(memberSpy.isNew()).thenReturn(true);
+
+        assertFalse(memberSpy.isSynced());
     }
 
     @Test
-    public void setUnsynced_validToken_setsTokenAndMarksAsUnsynced() throws Exception {
-        String token = "foo";
+    public void isSynced_isNotNewHasDirtyFields_returnsFalse() throws Exception {
+        Set<String> fields = new HashSet<>();
+        fields.add("foo");
 
-        member.setUnsynced(token);
+        when(memberSpy.getDirtyFields()).thenReturn(fields);
+        when(memberSpy.isNew()).thenReturn(false);
 
-        assertEquals(member.getToken(), token);
-        assertFalse(member.isSynced());
+        assertFalse(memberSpy.isSynced());
     }
 
     @Test
-    public void setSynced_isDirty() throws Exception {
+    public void isSynced_isNotNewHasNoDirtyFields_returnsTrue() throws Exception {
+        when(memberSpy.getDirtyFields()).thenReturn(new HashSet<>());
+        when(memberSpy.isNew()).thenReturn(false);
+
+        assertTrue(memberSpy.isSynced());
+    }
+
+    @Test
+    public void isNew_idIsNull_returnsTrue() throws Exception {
+        member.setId(null);
+
+        assertTrue(member.isNew());
+    }
+
+    @Test
+    public void isNew_idPresentAndDirty_returnsTrue() throws Exception {
+        when(memberSpy.dirty(Member.FIELD_NAME_ID)).thenReturn(true);
+
+        assertTrue(member.isNew());
+    }
+
+    @Test
+    public void isNew_idPresentAndNotDirtyDoesNotExistInDao_returnsTrue() throws Exception {
+        memberSpy.setId(id);
+
+        when(memberSpy.dirty(Member.FIELD_NAME_ID)).thenReturn(false);
+        when(mockDao.idExists(id)).thenReturn(false);
+
+        assertTrue(memberSpy.isNew());
+    }
+
+    @Test
+    public void isNew_idPresentAndNotDirtyAndExistsInDao_returnsFalse() throws Exception {
+        memberSpy.setId(id);
+
+        when(memberSpy.dirty(Member.FIELD_NAME_ID)).thenReturn(false);
+        when(mockDao.idExists(id)).thenReturn(true);
+
+        assertFalse(memberSpy.isNew());
+    }
+
+    @Test
+    public void changedFields_newModel_callsDiffFieldsWithNull() throws Exception {
+        Set<String> changeSet = new HashSet<>();
+
+        doReturn(true).when(memberSpy).isNew();
+        doReturn(changeSet).when(memberSpy).diffFields(null);
+
+        Set<String> result = memberSpy.changedFields();
+
+        assertEquals(changeSet, result);
+        verify(memberSpy, times(1)).diffFields(null);
+    }
+
+    @Test
+    public void changedFields_notNewModel_callsDiffFieldsWithThePersistedModel() throws Exception {
+        Set<String> changeSet = new HashSet<>();
+        Member mockPersistedMember = mock(Member.class);
+        memberSpy.setId(id);
+
+        doReturn(false).when(memberSpy).isNew();
+        when(mockDao.queryForId(id)).thenReturn(mockPersistedMember);
+        doReturn(changeSet).when(memberSpy).diffFields(mockPersistedMember);
+
+        Set<String> result = memberSpy.changedFields();
+
+        assertEquals(changeSet, result);
+        verify(memberSpy, times(1)).diffFields(mockPersistedMember);
+    }
+
+    @Test
+    public void diffFields_refModelIsNull_returnsAllFieldsSetOnModel() throws Exception {
+        member.setId(id);
+        member.setAge(15);
         member.setFullName("Foo");
 
+        Set<String> result = member.diffFields(null);
+
+        assertEquals(result.toString(), result.size(), 3);
+        assertTrue(result.contains("id"));
+        assertTrue(result.contains("age"));
+        assertTrue(result.contains("full_name"));
+    }
+
+    @Test
+    public void diffFields_refModelIsNotNull_returnsAllDuffFields() throws Exception {
+        member.setId(id);
+        member.setAge(15);
+        member.setGender(Member.GenderEnum.M);
+        member.setFullName("Foo");
+        Member refMember = new Member();
+        refMember.setId(id);
+        refMember.setGender(Member.GenderEnum.F);
+        refMember.setCardId("RWI123456");
+
+        Set<String> result = member.diffFields(refMember);
+
+        assertEquals(result.toString(), result.size(), 4);
+        assertTrue(result.contains("age"));
+        assertTrue(result.contains("full_name"));
+        assertTrue(result.contains("card_id"));
+        assertTrue(result.contains("gender"));
+    }
+
+    @Test
+    public void saveChanges_nullToken_throwsUnauthenticatedException() throws Exception {
         try {
-            member.setSynced();
-            fail("Should throw validation exception");
+            member.saveChanges(null);
+            fail("Should throw unauthenticated exception");
         } catch (Exception e) {
-            assertEquals(e.getMessage(), "full_name: Cannot mark dirty model as synced");
+            assertEquals(e.getMessage(), "Current user is not authenticated");
         }
     }
 
     @Test
-    public void setSynced_isNotDirty() throws Exception {
-        member.setIsNew(true);
-        member.setUnsynced("foo");
+    public void saveChanges_withToken_setsToken() throws Exception {
+        doReturn(new HashSet<>()).when(memberSpy).changedFields();
 
-        member.setSynced();
+        memberSpy.saveChanges(token);
 
-        assertFalse(member.isNew());
-        assertNull(member.getToken());
-        assertTrue(member.isSynced());
+        assertEquals(memberSpy.getToken(), token);
     }
 
     @Test
-    public void dirty() throws Exception {
-        String dirtyFieldName = Member.FIELD_NAME_FULL_NAME;
-        member.addDirtyField(dirtyFieldName);
+    public void saveChanges_withTokenNullId_setsId() throws Exception {
+        memberSpy.setId(null);
 
-        assertTrue(member.dirty(dirtyFieldName));
-        assertFalse(member.dirty(Member.FIELD_NAME_PHONE_NUMBER));
+        doReturn(new HashSet<>()).when(memberSpy).changedFields();
+
+        memberSpy.saveChanges(token);
+
+        assertNotNull(memberSpy.getId());
     }
 
     @Test
-    public void isDirty() throws Exception {
-        assertFalse(member.isDirty());
+    public void saveChanges_withToken_setsDirtyFieldsToChangedFields() throws Exception {
+        Set<String> changedFields = new HashSet<>();
 
-        member.addDirtyField(Member.FIELD_NAME_FULL_NAME);
+        doReturn(changedFields).when(memberSpy).changedFields();
 
-        assertTrue(member.isDirty());
+        memberSpy.saveChanges(token);
+
+        assertEquals(memberSpy.getDirtyFields(), changedFields);
     }
 
     @Test
-    public void clearDirtyFields() throws Exception {
-        member.addDirtyField(Member.FIELD_NAME_FULL_NAME);
-        assertTrue(member.isDirty());
+    public void saveChanges_withToken_persistsTheModel() throws Exception {
+        Set<String> changedFields = new HashSet<>();
 
-        member.clearDirtyFields();
-        assertFalse(member.isDirty());
+        doReturn(changedFields).when(memberSpy).changedFields();
+
+        memberSpy.saveChanges(token);
+
+        verify(mockDao, times(1)).createOrUpdate(memberSpy);
     }
 
     @Test
-    public void getTokenAuthHeaderString() throws Exception {
-        member.setToken("foo");
+    public void saveChanges_withToken_persistsAssociations() throws Exception {
+        Set<String> changedFields = new HashSet<>();
 
-        assertEquals(member.getTokenAuthHeaderString(), "Token foo");
+        doReturn(changedFields).when(memberSpy).changedFields();
+
+        memberSpy.saveChanges(token);
+
+        verify(memberSpy, times(1)).persistAssociations();
+    }
+
+    @Test
+    public void update_callsUpdateFromApiResponse() throws Exception {
+        memberSpy.updateFromSync();
+
+        verify(memberSpy, times(1)).handleUpdate();
+    }
+
+    @Test
+    public void updateFromSync_clearsDirtyFields() throws Exception {
+        memberSpy.updateFromSync();
+
+        verify(memberSpy, times(1)).clearDirtyFields();
+        assertTrue(memberSpy.getDirtyFields().size() == 0);
+    }
+
+    @Test
+    public void updateFromSync_persistsTheModel() throws Exception {
+        memberSpy.updateFromSync();
+
+        verify(mockDao, times(1)).createOrUpdate(memberSpy);
+    }
+
+    @Test
+    public void sync_nullToken_throwsUnauthenticatedException() throws Exception {
+        member.setToken(null);
+        try {
+            member.sync(mockContext);
+            fail("Should throw unauthenticated exception");
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "Current user is not authenticated");
+        }
+    }
+
+    @Test
+    public void sync_isNotDirty_throwsSyncException() throws Exception {
+        memberSpy.setToken(token);
+
+        when(memberSpy.isDirty()).thenReturn(false);
+
+        try {
+            memberSpy.sync(mockContext);
+            fail("Should throw unauthenticated exception");
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "Model does not have any fields that need to be synced");
+        }
+    }
+
+    @Test
+    public void sync_isNew_returnsTheResultOfPostRequest() throws Exception {
+        memberSpy.setToken(token);
+
+        when(memberSpy.isDirty()).thenReturn(true);
+        doReturn(true).when(memberSpy).isNew();
+        when(memberSpy.postApiCall(
+                mockContext, memberSpy.getToken(), BuildConfig.PROVIDER_ID, memberSpy))
+                .thenReturn(mockCall);
+        when(mockCall.execute()).thenReturn(mockResponse);
+
+        Response<Member> response = memberSpy.sync(mockContext);
+
+        assertEquals(response, mockResponse);
+    }
+
+    @Test
+    public void sync_isNotNew_returnsTheResultOfPatchRequest() throws Exception {
+        memberSpy.setId(id);
+        memberSpy.setToken(token);
+
+        when(memberSpy.isDirty()).thenReturn(true);
+        doReturn(false).when(memberSpy).isNew();
+        when(memberSpy.patchRequestBody(mockContext)).thenReturn(mockParams);
+        when(memberSpy.patchApiCall(mockContext, memberSpy.getToken(), id, mockParams))
+                .thenReturn(mockCall);
+        when(mockCall.execute()).thenReturn(mockResponse);
+
+        Response<Member> response = memberSpy.sync(mockContext);
+
+        assertEquals(response, mockResponse);
     }
 }
