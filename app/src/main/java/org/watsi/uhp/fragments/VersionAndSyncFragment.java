bb16545e48c914be3d88dbfe067cdba31699ca61
Michael Liang <liangmicha@gmail.com>
2017-06-07 17:40:45 -0700

    Get rid of Log statement
    [no tracker]

--- app/src/main/java/org/watsi/uhp/fragments/VersionAndSyncFragment.java
+++ app/src/main/java/org/watsi/uhp/fragments/VersionAndSyncFragment.java
@@ -5,7 +5,6 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
