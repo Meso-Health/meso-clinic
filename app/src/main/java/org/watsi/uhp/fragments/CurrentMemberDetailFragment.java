3851b631507ad6d642d44a6d77641458209ed8ce
Michael Liang <liangmicha@gmail.com>
2017-06-13 12:38:29 -0700

    Fixed ordred in currentMemberDetailFragment
    [no tracker]

--- app/src/main/java/org/watsi/uhp/fragments/CurrentMemberDetailFragment.java
+++ app/src/main/java/org/watsi/uhp/fragments/CurrentMemberDetailFragment.java
@@ -53,7 +53,7 @@ public class CurrentMemberDetailFragment extends MemberDetailFragment {
 
     @Override
     protected void setBottomListView(View view) {
-
+        // no-op
     }
 
     @Override
