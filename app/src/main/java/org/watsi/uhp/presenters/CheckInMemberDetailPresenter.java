a1a33d9ae59228bd83b26e39ff836b36baf93a42
Michael Liang <liangmicha@gmail.com>
2017-06-14 21:58:40 -0700

    Optimize imports
    [no tracker]

--- app/src/main/java/org/watsi/uhp/presenters/CheckInMemberDetailPresenter.java
+++ app/src/main/java/org/watsi/uhp/presenters/CheckInMemberDetailPresenter.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
