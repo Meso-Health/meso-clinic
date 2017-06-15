93d4e7015eb5c076988a64930307ac504abac6bb
Michael Liang <liangmicha@gmail.com>
2017-06-14 18:05:49 -0700

    Remove line about gettingcurrent logged in suer
    [no tracker]

--- app/src/main/java/org/watsi/uhp/managers/SessionManager.java
+++ app/src/main/java/org/watsi/uhp/managers/SessionManager.java
@@ -38,10 +38,6 @@ public class SessionManager {
         ExceptionManager.setPersonData(String.valueOf(user.getId()), user.getUsername());
     }
 
-    public String getCurrentLoggedInUsername() {
-        return mPreferencesManager.getUsername();
-    }
-
     /**
      * Returns a future that resolves to a Bundle containing the logged-in users's
      * authentication token if one is stored or an Intent to launch an activity
