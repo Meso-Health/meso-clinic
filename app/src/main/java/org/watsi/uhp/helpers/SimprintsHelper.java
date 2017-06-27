445c3892ecc7b084ee5f52a9135287ecc8bc47dc
Michael Liang <liangmicha@gmail.com>
2017-06-27 14:20:40 +0300

    undo fixes simprints
    [no tracker]

--- app/src/main/java/org/watsi/uhp/helpers/SimprintsHelper.java
+++ app/src/main/java/org/watsi/uhp/helpers/SimprintsHelper.java
@@ -47,7 +47,7 @@ public class SimprintsHelper {
     }
 
     public void verify(String providerId, UUID fingerprintsGuid) throws SimprintsHelperException {
-        Intent fingerprintVerificationIntent = getSimHelper().verify(providerId, fingerprintsGuid.toString() + "a");
+        Intent fingerprintVerificationIntent = getSimHelper().verify(providerId, fingerprintsGuid.toString());
         if (validIntent(fingerprintVerificationIntent)) {
             mFragment.startActivityForResult(
                     fingerprintVerificationIntent,
