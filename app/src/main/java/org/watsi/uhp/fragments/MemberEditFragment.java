17c2c2c19ea957760a03cbb51a6979d91271d65b
Michael Liang <liangmicha@gmail.com>
2017-07-30 14:11:16 -0700

    Deleted duplicate code in MemberEditFragment
    [no tracker]

--- app/src/main/java/org/watsi/uhp/fragments/MemberEditFragment.java
+++ app/src/main/java/org/watsi/uhp/fragments/MemberEditFragment.java
@@ -1,20 +1,16 @@
 package org.watsi.uhp.fragments;
 
 import android.content.DialogInterface;
-import android.databinding.BaseObservable;
-import android.databinding.Bindable;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Toast;
 
-import org.watsi.uhp.BR;
 import org.watsi.uhp.R;
 import org.watsi.uhp.databinding.FragmentMemberEditBinding;
 import org.watsi.uhp.listeners.SetBarcodeFragmentListener;
 import org.watsi.uhp.managers.ExceptionManager;
 import org.watsi.uhp.managers.NavigationManager;
-import org.watsi.uhp.models.AbstractModel;
 import org.watsi.uhp.models.IdentificationEvent;
 import org.watsi.uhp.models.Member;
 import org.watsi.uhp.view_models.MemberEditViewModel;
@@ -81,149 +77,5 @@ public class MemberEditFragment extends FormFragment<Member> {
         view.findViewById(R.id.scan_card).setOnClickListener(new SetBarcodeFragmentListener(
                 getNavigationManager(), BarcodeFragment.ScanPurposeEnum.MEMBER_EDIT,
                 mSyncableModel, idEvent));
-    }
-
-    public class MemberEditFragmentMemberView extends BaseObservable {
-        private Member mMember;
-        private String fullName;
-        private String fullNameError;
-        private String phoneNumber;
-        private String phoneNumberError;
-        private String cardId;
-        private String cardIdError;
-        private boolean saveEnabled;
-
-        public MemberEditFragmentMemberView(Member member) {
-            mMember = member;
-
-            fullName = member.getFullName();
-            fullNameError = null;
-            phoneNumber = member.getPhoneNumber();
-            phoneNumberError = null;
-            cardId = member.getCardId();
-            cardIdError = null;
-
-            updateSaveButton();
-        }
-
-        @Bindable
-        public String getFullName() { return fullName; }
-
-        @Bindable
-        public String getFullNameError() {
-            return fullNameError;
-        }
-
-        @Bindable
-        public String getPhoneNumber() {
-            return phoneNumber;
-        }
-
-        @Bindable
-        public String getPhoneNumberError() {
-            return phoneNumberError;
-        }
-
-        @Bindable
-        public String getCardId() {
-            return cardId;
-        }
-
-        @Bindable
-        public String getCardIdError() {
-            return cardIdError;
-        }
-
-        @Bindable
-        public boolean getSaveEnabled() {
-            return saveEnabled;
-        }
-
-        @Bindable
-        public void setFullName(String fullName) {
-            this.fullName = fullName;
-            notifyPropertyChanged(BR.fullName);
-            validateFullName();
-            updateSaveButton();
-            try {
-                mMember.setFullName(fullName);
-            } catch (AbstractModel.ValidationException e) {
-                ExceptionManager.reportException(e);
-            }
-        }
-
-        @Bindable
-        public void setPhoneNumber(String phoneNumber) {
-            this.phoneNumber = phoneNumber;
-            notifyPropertyChanged(BR.phoneNumber);
-            try {
-                mMember.setPhoneNumber(phoneNumber);
-            } catch (AbstractModel.ValidationException e) {
-                ExceptionManager.reportException(e);
-            }
-        }
-
-        public void onClickSave() {
-            if (validateEverything()) {
-                nextStep(getView());
-            }
-        }
-
-        private boolean validateEverything() {
-            return validateFullName() && validatePhoneNumber() && validateCardId();
-        }
-
-        private void updateSaveButton() {
-            saveEnabled = validFullName() && validPhoneNumber() && validCardId();
-            notifyPropertyChanged(BR.saveEnabled);
-        }
-
-        private boolean validPhoneNumber() {
-            return phoneNumber == null || phoneNumber.isEmpty() || Member.validPhoneNumber(phoneNumber);
-        }
-
-        private boolean validCardId() {
-            return Member.validCardId(cardId);
-        }
-
-        private boolean validFullName() {
-            return fullName != null && !fullName.isEmpty();
-        }
-
-        private boolean validateFullName() {
-            boolean success = true;
-            if (validFullName()) {
-                this.fullNameError = null;
-            } else {
-                this.fullNameError = getString(R.string.name_validation_error);
-                success = false;
-            }
-            notifyPropertyChanged(BR.fullNameError);
-            return success;
-        }
-
-        private boolean validatePhoneNumber() {
-            boolean success = true;
-            if (validPhoneNumber()) {
-                this.phoneNumberError = null;
-            } else {
-                this.phoneNumberError = getString(R.string.phone_number_validation_error);
-                success = false;
-            }
-            notifyPropertyChanged(BR.phoneNumberError);
-            return success;
-        }
-
-        private boolean validateCardId() {
-            boolean success = true;
-            if (validCardId()) {
-                this.cardIdError = null;
-            } else {
-                this.cardIdError = getString(R.string.card_id_validation_error);
-                success = false;
-            }
-            notifyPropertyChanged(BR.cardIdError);
-            return success;
-        }
     }
 }
