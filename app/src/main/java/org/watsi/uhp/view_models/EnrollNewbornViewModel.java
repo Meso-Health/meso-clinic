75200998b2005a67cdd12205e59ecab956b918c1
Michael Liang <liangmicha@gmail.com>
2017-07-30 15:36:50 -0700

    Fixing EnrollNewbornViewModel library
    [no tracker]

--- app/src/main/java/org/watsi/uhp/view_models/EnrollNewbornViewModel.java
+++ app/src/main/java/org/watsi/uhp/view_models/EnrollNewbornViewModel.java
@@ -2,7 +2,6 @@ package org.watsi.uhp.view_models;
 
 import android.databinding.Bindable;
 import android.support.annotation.IdRes;
-import android.util.Log;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -12,31 +11,25 @@ import org.watsi.uhp.fragments.FormFragment;
 import org.watsi.uhp.models.Member;
 
 public class EnrollNewbornViewModel extends MemberEditViewModel {
+
     public EnrollNewbornViewModel(FormFragment<Member> formFragment, Member member) {
         super(formFragment, member);
     }
 
     @Bindable
-    public boolean getIsMale() {
-        return getMember().getGender() == Member.GenderEnum.M;
-    }
+    public boolean getIsMale() { return getMember().getGender() == Member.GenderEnum.M; }
 
     @Bindable
-    public boolean getIsFemale() {
-        return getMember().getGender() == Member.GenderEnum.F;
-    }
+    public boolean getIsFemale() { return getMember().getGender() == Member.GenderEnum.F; }
 
     public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
-        Log.i("UHP-debug", "onCheckedChanged is called");
         RadioButton selectedRadio =
                 (RadioButton) radioGroup.findViewById(radioGroup.getCheckedRadioButtonId());
         if (selectedRadio.getId() == (R.id.male)) {
-            // selected male
             getMember().setGender(Member.GenderEnum.M);
             notifyPropertyChanged(BR.isMale);
             notifyPropertyChanged(BR.isFemale);
         } else {
-            // selected female
             getMember().setGender(Member.GenderEnum.F);
             notifyPropertyChanged(BR.isMale);
             notifyPropertyChanged(BR.isFemale);
@@ -51,9 +44,7 @@ public class EnrollNewbornViewModel extends MemberEditViewModel {
         }
     }
 
-    boolean validGender() {
-        return getMember().getGender() != null;
-    }
+    boolean validGender() { return getMember().getGender() != null; }
 
     void updateSaveButton() {
         setSaveEnabled(validFullName() && validPhoneNumber() && validCardId() && validGender());
