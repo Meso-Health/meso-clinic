196fe1b574f546ea9ce063c2536f819737dac07d
Michael Liang <liangmicha@gmail.com>
2017-08-01 13:14:37 -0700

    Fix the barcode fragment
    [no tracker]

--- app/src/test/java/org/watsi/uhp/listeners/SetBarcodeFragmentListenerTest.kt
+++ app/src/test/java/org/watsi/uhp/listeners/SetBarcodeFragmentListenerTest.kt
@@ -1,12 +1,12 @@
 package org.watsi.uhp.listeners
 
-import android.os.Bundle
 import android.view.View
 import org.junit.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.times
 import org.watsi.uhp.fragments.BarcodeFragment
 import org.watsi.uhp.managers.NavigationManager
+import org.watsi.uhp.models.IdentificationEvent
 import org.watsi.uhp.models.Member
 
 class SetBarcodeFragmentListenerTest {
@@ -15,16 +15,16 @@ class SetBarcodeFragmentListenerTest {
     val mockNavigationManager = Mockito.mock(NavigationManager::class.java)
     val scanPurpose = BarcodeFragment.ScanPurposeEnum.ID
     val mockMember = Mockito.mock(Member::class.java)
-    val mockBundle = Mockito.mock(Bundle::class.java)
+    val mockIdEvent = Mockito.mock(IdentificationEvent::class.java)
 
     @Test
     fun onClick() {
         val listener = SetBarcodeFragmentListener(
-                mockNavigationManager, scanPurpose, mockMember, mockBundle)
+                mockNavigationManager, scanPurpose, mockMember, mockIdEvent)
 
         listener.onClick(mockView)
 
         Mockito.verify(mockNavigationManager, times(1)).setBarcodeFragment(
-                scanPurpose, mockMember, mockBundle)
+                scanPurpose, mockMember, mockIdEvent)
     }
 }
