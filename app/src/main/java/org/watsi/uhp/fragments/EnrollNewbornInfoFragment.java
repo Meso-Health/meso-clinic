140de0137eb8f756aab7a0b75de745a6504935e4
Michael Liang <liangmicha@gmail.com>
2017-09-07 23:40:28 +0200

    Added a new component for datepicker as well as unit tests
    [no tracker]

--- app/src/main/java/org/watsi/uhp/fragments/EnrollNewbornInfoFragment.java
+++ app/src/main/java/org/watsi/uhp/fragments/EnrollNewbornInfoFragment.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.DatePicker;
 
 import org.watsi.uhp.R;
+import org.watsi.uhp.custom_components.NewbornBirthdateDatePicker;
 import org.watsi.uhp.databinding.FragmentEnrollNewbornBinding;
 import org.watsi.uhp.listeners.SetBarcodeFragmentListener;
 import org.watsi.uhp.managers.NavigationManager;
@@ -12,17 +13,11 @@ import org.watsi.uhp.models.IdentificationEvent;
 import org.watsi.uhp.models.Member;
 import org.watsi.uhp.view_models.EnrollNewbornViewModel;
 
-import java.util.Calendar;
-
-import static org.watsi.uhp.helpers.DateTimeHelper.makeCalendarThreeMonthsAgo;
-import static org.watsi.uhp.helpers.DateTimeHelper.makeCalendarTomorrow;
-import static org.watsi.uhp.helpers.DateTimeHelper.makeCalendarToday;
-
 public class EnrollNewbornInfoFragment extends FormFragment<Member> {
 
     private IdentificationEvent mIdEvent;
     private View mView;
-    private DatePicker mDatePicker;
+    private NewbornBirthdateDatePicker mNewbornBirthdateDatePicker;
 
     @Override
     int getTitleLabelId() {
@@ -46,16 +41,13 @@ public class EnrollNewbornInfoFragment extends FormFragment<Member> {
 
     @Override
     void setUpFragment(View view) {
-        mView = view;
-        mDatePicker = (DatePicker) mView.findViewById(R.id.birthdate);
-
         FragmentEnrollNewbornBinding binding = DataBindingUtil.bind(view);
         EnrollNewbornViewModel enrollNewbornViewModel = new EnrollNewbornViewModel(this, mSyncableModel);
         binding.setMember(enrollNewbornViewModel);
 
-
+        mView = view;
         mIdEvent = (IdentificationEvent) getArguments().getSerializable(NavigationManager.IDENTIFICATION_EVENT_BUNDLE_FIELD);
-        setUpDatePicker();
+        mNewbornBirthdateDatePicker = new NewbornBirthdateDatePicker((DatePicker) mView.findViewById(R.id.birthdate), mSyncableModel);
         setUpScanCardListener();
     }
 
@@ -63,37 +55,5 @@ public class EnrollNewbornInfoFragment extends FormFragment<Member> {
         mView.findViewById(R.id.scan_card).setOnClickListener(new SetBarcodeFragmentListener(
                 getNavigationManager(), BarcodeFragment.ScanPurposeEnum.NEWBORN,
                 mSyncableModel, mIdEvent));
-    }
-
-
-    void setUpDatePicker() {
-        Calendar cal = makeCalendarToday();
-        if (mSyncableModel.getBirthdate() != null) {
-            cal.setTime(mSyncableModel.getBirthdate());
-        } else {
-            mSyncableModel.setBirthdate(cal.getTime());
-        }
-
-        mDatePicker.init(
-                cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH),
-                new DatePicker.OnDateChangedListener() {
-                    @Override
-                    public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
-                        Calendar cal = makeCalendarToday();
-                        cal.set(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth());
-                        mSyncableModel.setBirthdate(cal.getTime());
-                    }
-                }
-        );
-        // This is a policy decision we made during IPM so only newborns born within the last
-        // three months can be enrolled via this flow.
-        mDatePicker.setMinDate(makeCalendarThreeMonthsAgo().getTimeInMillis());
-        mDatePicker.setMaxDate(makeCalendarTomorrow().getTimeInMillis());
-    }
-
-    public DatePicker getDatePicker() {
-        return mDatePicker;
     }
 }
---
+++ app/src/test/java/org/watsi/uhp/custom_components/NewbornBirthdateDatePickerTest.java
@@ -0,0 +1,116 @@
+package org.watsi.uhp.custom_components;
+
+import android.content.Context;
+import android.widget.DatePicker;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.watsi.uhp.BuildConfig;
+import org.watsi.uhp.MemberFactory;
+import org.watsi.uhp.activities.ClinicActivity;
+import org.watsi.uhp.models.Member;
+
+import java.util.Calendar;
+
+import static junit.framework.Assert.assertEquals;
+
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class NewbornBirthdateDatePickerTest {
+    private Member mMember;
+    private DatePicker mDatePicker;
+
+    @Before
+    public void setUp() throws Exception {
+        mMember = new MemberFactory("Parent", "RWI123123", 30, Member.GenderEnum.F);
+        Context context = Robolectric.buildActivity(ClinicActivity.class).create().get();
+        mDatePicker = new DatePicker(context);
+    }
+
+    @Test
+    public void datePicker_maxDateSet() throws Exception {
+        NewbornBirthdateDatePicker newbornBirthdateDatePicker = new NewbornBirthdateDatePicker(mDatePicker, mMember);
+        Calendar maxDate = Calendar.getInstance();
+        maxDate.setTimeInMillis(mDatePicker.getMaxDate());
+
+        Calendar today = Calendar.getInstance();
+
+        assertEquals(maxDate.get(Calendar.YEAR), today.get(Calendar.YEAR));
+        assertEquals(maxDate.get(Calendar.MONTH), today.get(Calendar.MONTH));
+        assertEquals(maxDate.get(Calendar.DAY_OF_MONTH), today.get(Calendar.DAY_OF_MONTH));
+        assertDateHasNoTime(mDatePicker.getMaxDate());
+    }
+
+    @Test
+    public void datePicker_minDateSet() throws Exception {
+        NewbornBirthdateDatePicker newbornBirthdateDatePicker = new NewbornBirthdateDatePicker(mDatePicker, mMember);
+        Calendar threeMonthsAgo = Calendar.getInstance();
+        threeMonthsAgo.add(Calendar.MONTH, -3);
+
+        Calendar minDate = Calendar.getInstance();
+        minDate.setTimeInMillis(mDatePicker.getMinDate());
+
+        assertEquals(minDate.get(Calendar.YEAR), threeMonthsAgo.get(Calendar.YEAR));
+        assertEquals(minDate.get(Calendar.MONTH), threeMonthsAgo.get(Calendar.MONTH));
+        assertEquals(minDate.get(Calendar.DAY_OF_MONTH), threeMonthsAgo.get(Calendar.DAY_OF_MONTH));
+        assertDateHasNoTime(mDatePicker.getMinDate());
+    }
+
+    @Test
+    public void datePicker_memberNoBirthdateDefaultsToToday() throws Exception {
+        mMember.setBirthdate(null);
+        NewbornBirthdateDatePicker newbornBirthdateDatePicker = new NewbornBirthdateDatePicker(mDatePicker, mMember);
+        Calendar today = Calendar.getInstance();
+        assertEquals(mDatePicker.getYear(), today.get(Calendar.YEAR));
+        assertEquals(mDatePicker.getMonth(), today.get(Calendar.MONTH));
+        assertEquals(mDatePicker.getDayOfMonth(), today.get(Calendar.DAY_OF_MONTH));
+
+        assertDateHasNoTime(mMember.getBirthdate().getTime());
+    }
+
+    @Test
+    public void datePicker_memberHasBirthdate() throws Exception {
+        NewbornBirthdateDatePicker newbornBirthdateDatePicker = new NewbornBirthdateDatePicker(mDatePicker, mMember);
+        Calendar birthdate = Calendar.getInstance();
+        birthdate.setTime(mMember.getBirthdate());
+        assertEquals(mDatePicker.getYear(), birthdate.get(Calendar.YEAR));
+        assertEquals(mDatePicker.getMonth(), birthdate.get(Calendar.MONTH));
+        assertEquals(mDatePicker.getDayOfMonth(), birthdate.get(Calendar.DAY_OF_MONTH));
+
+        assertDateHasNoTime(mMember.getBirthdate().getTime());
+    }
+
+
+    @Test
+    public void datePicker_onDateChanged() throws Exception {
+        NewbornBirthdateDatePicker newbornBirthdateDatePicker = new NewbornBirthdateDatePicker(mDatePicker, mMember);
+        Calendar fiveDaysAgo = Calendar.getInstance();
+        fiveDaysAgo.add(Calendar.DAY_OF_MONTH, -5);
+
+        mDatePicker.updateDate(
+                fiveDaysAgo.get(Calendar.YEAR),
+                fiveDaysAgo.get(Calendar.MONTH),
+                fiveDaysAgo.get(Calendar.DAY_OF_MONTH)
+        );
+
+        assertEquals(mDatePicker.getYear(), fiveDaysAgo.get(Calendar.YEAR));
+        assertEquals(mDatePicker.getMonth(), fiveDaysAgo.get(Calendar.MONTH));
+        assertEquals(mDatePicker.getDayOfMonth(), fiveDaysAgo.get(Calendar.DAY_OF_MONTH));
+
+        assertDateHasNoTime(mMember.getBirthdate().getTime());
+    }
+
+    private void assertDateHasNoTime(long timeInMilliseconds) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(timeInMilliseconds);
+        assertEquals(calendar.get(Calendar.HOUR), 0);
+        assertEquals(calendar.get(Calendar.MINUTE), 0);
+        assertEquals(calendar.get(Calendar.SECOND), 0);
+        assertEquals(calendar.get(Calendar.MILLISECOND), 0);
+    }
+}
