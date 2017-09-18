db14533ff2053687ac690e8e198b8604bf6271d7
Michael Liang <liangmicha@gmail.com>
2017-09-18 20:35:22 +0200

    Removed unused log statement in ClinicActivity
    [no tracker]

--- app/src/main/java/org/watsi/uhp/activities/ClinicActivity.java
+++ app/src/main/java/org/watsi/uhp/activities/ClinicActivity.java
@@ -16,7 +16,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
