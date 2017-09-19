09c65c9777790cff696f23d29198758d597bb88f
Michael Liang <liangmicha@gmail.com>
2017-09-19 14:44:25 -0700

    Added base fragment logging temperarily
    [no tracker]

--- app/src/main/java/org/watsi/uhp/fragments/BaseFragment.java
+++ app/src/main/java/org/watsi/uhp/fragments/BaseFragment.java
@@ -1,8 +1,10 @@
 package org.watsi.uhp.fragments;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import org.watsi.uhp.activities.ClinicActivity;
 import org.watsi.uhp.managers.NavigationManager;
@@ -37,5 +39,33 @@ public abstract class BaseFragment extends Fragment {
 
     public String getName() {
         return getClass().getSimpleName();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i("UHP-debug", "resumed " + this.getName());
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        int count = fm.getBackStackEntryCount();
+        String s = "Back stack currently contains: \n";
+        for (int i=count-1; i >= 0; i--) {
+            FragmentManager.BackStackEntry entry = fm.getBackStackEntryAt(i);
+            s = s + " " + entry.getName() + " \n";
+        }
+        Log.i("UHP-debug", s);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i("UHP-debug", "onPause " + this.getName());
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        int count = fm.getBackStackEntryCount();
+        String s = "Back stack currently contains: \n";
+        for (int i=count-1; i >= 0; i--) {
+            FragmentManager.BackStackEntry entry = fm.getBackStackEntryAt(i);
+            s = s + " " + entry.getName() + " \n";
+        }
+        Log.i("UHP-debug", s);
     }
 }
