2f01c523decb4bbac0a52be4e797e7b2e737fe77
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2018-04-20 11:33:11 -0700

    Refactor CurrentPatientsFragment

---
+++ app/src/main/java/org/watsi/uhp/fragments/CurrentPatientsFragment.kt
@@ -0,0 +1,65 @@
+package org.watsi.uhp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_current_patients.current_patients
+import kotlinx.android.synthetic.main.fragment_current_patients.current_patients_label
+import kotlinx.android.synthetic.main.fragment_current_patients.identification_button
+
+import org.watsi.domain.entities.Member
+import org.watsi.domain.repositories.IdentificationEventRepository
+import org.watsi.domain.repositories.MemberRepository
+import org.watsi.uhp.R
+import org.watsi.uhp.adapters.MemberAdapter
+import org.watsi.uhp.managers.NavigationManager
+
+import javax.inject.Inject
+
+class CurrentPatientsFragment : DaggerFragment() {
+
+    @Inject lateinit var navigationManager: NavigationManager
+    @Inject lateinit var memberRepository: MemberRepository
+    @Inject lateinit var identificationEventRepository: IdentificationEventRepository
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        activity.setTitle(R.string.current_patients_fragment_label)
+        setHasOptionsMenu(true)
+        return inflater?.inflate(R.layout.fragment_current_patients, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+
+        val checkedInMembers = memberRepository.checkedInMembers()
+
+        if (checkedInMembers.isEmpty()) {
+            current_patients_label.visibility = View.GONE
+        } else {
+            current_patients_label.text = activity.resources.getQuantityString(
+                    R.plurals.current_patients_label, checkedInMembers.size, checkedInMembers.size)
+
+            current_patients.adapter = MemberAdapter(context,
+                    checkedInMembers,
+                                                     true,
+                                                     identificationEventRepository)
+
+            current_patients.onItemClickListener = AdapterView.OnItemClickListener { parent, _, position, _ ->
+                val member = parent.getItemAtPosition(position) as Member
+                // TODO: navigate to MemberDetailFragment
+            }
+        }
+
+        identification_button.setOnClickListener {
+            // TODO: navigate to BarcodeFragment
+        }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?) {
+        menu!!.findItem(R.id.menu_logout).isVisible = true
+        menu.findItem(R.id.menu_version).isVisible = true
+    }
+}
