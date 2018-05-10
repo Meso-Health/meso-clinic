96da190dc3a23c96431fadb7d6967d333449da29
Michael Liang <liangmicha@gmail.com>
2018-05-10 16:40:52 -0700

    Implement the DeltaDao methods related to syncing forreal

--- device/src/main/java/org/watsi/device/db/repositories/DeltaRepositoryImpl.kt
+++ device/src/main/java/org/watsi/device/db/repositories/DeltaRepositoryImpl.kt
@@ -15,9 +15,24 @@ class DeltaRepositoryImpl(
         private val clock: Clock
 ) : DeltaRepository {
     override fun syncStatus(): Flowable<DeltaRepository.SyncStatus> {
-        // TODO implememnt these on deltaDao.
-        return Flowable.just(DeltaRepository.SyncStatus(1, 2, 2,3, 4))
-    }
+        val syncFlowables = listOf(
+                deltaDao.unsyncedCount(Delta.ModelName.MEMBER, Delta.Action.EDIT),
+                deltaDao.unsyncedCount(Delta.ModelName.MEMBER, Delta.Action.ADD),
+                deltaDao.unsyncedCount(Delta.ModelName.IDENTIFICATION_EVENT),
+                deltaDao.unsyncedCount(Delta.ModelName.ENCOUNTER),
+                deltaDao.unsyncedCount(Delta.ModelName.ENCOUNTER_FORM)
+        )
+
+        return Flowable.zip(syncFlowables, { results ->
+            DeltaRepository.SyncStatus(
+                    results[0] as Int,
+                    results[1] as Int,
+                    results[2] as Int,
+                    results[3] as Int,
+                    results[4] as Int
+            )
+        })
+}
 
     override fun unsynced(modelName: Delta.ModelName): Single<List<Delta>> {
         return deltaDao.unsynced(modelName).map { deltaModels ->
