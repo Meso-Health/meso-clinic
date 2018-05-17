39c92c5b17d87c1fdcc3953f3b260e5833931dc6
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2018-05-16 18:05:21 -0700

    Fix bug in fetch logic
    Do not overwrite data for unsynced members

--- device/src/main/java/org/watsi/device/db/repositories/MemberRepositoryImpl.kt
+++ device/src/main/java/org/watsi/device/db/repositories/MemberRepositoryImpl.kt
@@ -45,13 +45,16 @@ class MemberRepositoryImpl(private val memberDao: MemberDao,
             api.members(token.getHeaderString(),
                         token.user.providerId).flatMapCompletable { fetchedMembers ->
                 memberDao.unsynced().flatMapCompletable { unsyncedMembers ->
+                    val unsyncedIds = unsyncedMembers.map { it.id }
                     memberDao.allSingle().flatMapCompletable { persistedMembers ->
                         val membersById = persistedMembers.groupBy { it.id }
                         Completable.fromAction {
-                            val fetchedAndUnsyncedIds =
-                                    fetchedMembers.map { it.id } + unsyncedMembers.map { it.id }
-                            memberDao.deleteNotInList(fetchedAndUnsyncedIds)
-                            memberDao.upsert(fetchedMembers.map { memberApi ->
+                            val fetchedAndUnsyncedIds = fetchedMembers.map { it.id } + unsyncedIds
+                            memberDao.deleteNotInList(fetchedAndUnsyncedIds.distinct())
+                            val fetchedMembersWithoutUnsynced = fetchedMembers.filter {
+                                !unsyncedIds.contains(it.id)
+                            }
+                            memberDao.upsert(fetchedMembersWithoutUnsynced.map { memberApi ->
                                 val persistedMember = membersById[memberApi.id]?.firstOrNull()?.toMember()
                                 MemberModel.fromMember(memberApi.toMember(persistedMember), clock)
                             })
