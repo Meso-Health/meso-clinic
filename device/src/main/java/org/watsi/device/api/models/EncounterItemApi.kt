cb4dd0fbfadd52414d0f269e04d82818b7f5ecb7
Byron Zhang <byron.yi.zhang@gmail.com>
2018-05-17 11:14:46 -0700

    Fix billable api.
    * current logic assumes that all billables are NEW, which is blowing up when hitting the backend.
    * for now assume that all billables are EXISTING (NOT NEW) instead, to get syncing working at a baseline level.
    
    [no tracker]

--- device/src/main/java/org/watsi/device/api/models/EncounterItemApi.kt
+++ device/src/main/java/org/watsi/device/api/models/EncounterItemApi.kt
@@ -1,37 +1,20 @@
 package org.watsi.device.api.models
 
 import com.google.gson.annotations.SerializedName
-import org.watsi.domain.entities.Billable
 import org.watsi.domain.relations.EncounterItemWithBillable
 import java.util.UUID
 
 data class EncounterItemApi(
         @SerializedName("id") val id: UUID,
         @SerializedName("encounter_id") val encounterId: UUID,
-        @SerializedName("billable") val billable: BillableApi,
+        @SerializedName("billable_id") val billable_id: UUID,
         @SerializedName("quantity") val quantity: Int
 ) {
 
     constructor (encounterItemWithBillable: EncounterItemWithBillable) :
             this(id = encounterItemWithBillable.encounterItem.id,
                  encounterId = encounterItemWithBillable.encounterItem.encounterId,
-                 billable = BillableApi(
-                         encounterItemWithBillable.billable.id,
-                         encounterItemWithBillable.billable.type,
-                         encounterItemWithBillable.billable.composition,
-                         encounterItemWithBillable.billable.unit,
-                         encounterItemWithBillable.billable.price,
-                         encounterItemWithBillable.billable.name
-                 ),
+                 billable_id = encounterItemWithBillable.billable.id,
                  quantity = encounterItemWithBillable.encounterItem.quantity
             )
-
-    data class BillableApi(
-            @SerializedName("id") val id: UUID,
-            @SerializedName("type") val type: Billable.Type,
-            @SerializedName("composition") val composition: String?,
-            @SerializedName("unit") val unit: String?,
-            @SerializedName("price") val price: Int,
-            @SerializedName("name") val name: String
-    )
 }
