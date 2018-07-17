24777ac3f16d1fd025dda80369e8b23661e1f61c
katrinacrisostomo <katrina@watsi.org>
2018-07-17 02:10:26 -0400

    Add viewModel, new fields and onTextChange listeners to fields
    [#158960089]

--- app/src/ethiopia/java/org/watsi/uhp/fragments/NewClaimFragment.kt
+++ app/src/ethiopia/java/org/watsi/uhp/fragments/NewClaimFragment.kt
@@ -1,6 +1,8 @@
 package org.watsi.uhp.fragments
 
 import android.app.AlertDialog
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -8,20 +10,33 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import dagger.android.support.DaggerFragment
-import kotlinx.android.synthetic.ethiopia.fragment_new_claim.membership_number
+import kotlinx.android.synthetic.ethiopia.fragment_new_claim.region_number
+import kotlinx.android.synthetic.ethiopia.fragment_new_claim.woreda_number
+import kotlinx.android.synthetic.ethiopia.fragment_new_claim.kebele_number
+import kotlinx.android.synthetic.ethiopia.fragment_new_claim.member_status
+import kotlinx.android.synthetic.ethiopia.fragment_new_claim.household_number
+import kotlinx.android.synthetic.ethiopia.fragment_new_claim.household_member_number
 import kotlinx.android.synthetic.ethiopia.fragment_new_claim.start_button
-import org.watsi.device.managers.Logger
 import org.watsi.device.managers.SessionManager
 import org.watsi.uhp.R
 import org.watsi.uhp.activities.ClinicActivity
+import org.watsi.uhp.helpers.LayoutHelper
 import org.watsi.uhp.managers.NavigationManager
+import org.watsi.uhp.viewmodels.NewClaimViewModel
 import javax.inject.Inject
 
 class NewClaimFragment : DaggerFragment() {
 
     @Inject lateinit var navigationManager: NavigationManager
     @Inject lateinit var sessionManager: SessionManager
-    @Inject lateinit var logger: Logger
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+    lateinit var viewModel: NewClaimViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(NewClaimViewModel::class.java)
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         (activity as ClinicActivity).setToolbar(context.getString(R.string.new_claim_fragment_label), null)
@@ -30,16 +45,25 @@ class NewClaimFragment : DaggerFragment() {
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-        start_button.setOnClickListener {
-            val membershipNumber = membership_number.getText().toString()
+        region_number.addTextChangedListener(LayoutHelper.OnChangedListener {
+            text -> viewModel.onRegionNumberChange(text)
+        })
 
-            if (membershipNumber.isBlank()) {
-                // TODO: disable button when membership number field is blank
-                logger.error("no membership number")
-            } else {
-                navigationManager.goTo(MemberInformationFragment.withMembershipNumber(membershipNumber))
-            }
-        }
+        woreda_number.addTextChangedListener(LayoutHelper.OnChangedListener {
+            text -> viewModel.onWoredaNumberChange(text)
+        })
+
+        kebele_number.addTextChangedListener(LayoutHelper.OnChangedListener {
+            text -> viewModel.onKebeleNumberChange(text)
+        })
+
+        household_number.addTextChangedListener(LayoutHelper.OnChangedListener {
+            text -> viewModel.onHouseholdNumberChange(text)
+        })
+
+        household_member_number.addTextChangedListener(LayoutHelper.OnChangedListener {
+            text -> viewModel.onHouseholdMemberNumberChange(text)
+        })
     }
 
     override fun onPrepareOptionsMenu(menu: Menu?) {
@@ -68,5 +92,4 @@ class NewClaimFragment : DaggerFragment() {
         }
         return true
     }
-
 }
