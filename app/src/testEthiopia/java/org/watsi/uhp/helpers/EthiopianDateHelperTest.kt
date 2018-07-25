d9f47a64233114df799d9d5d34a9cbc27e1f237a
Jeff Beorse <jeff@watsi.org>
2018-07-25 15:06:28 -0700

    Fix JUnit package

--- app/src/testEthiopia/java/org/watsi/uhp/helpers/EthiopianDateHelperTest.kt
+++ app/src/testEthiopia/java/org/watsi/uhp/helpers/EthiopianDateHelperTest.kt
@@ -1,6 +1,6 @@
 package org.watsi.uhp.helpers
 
-import junit.framework.Assert.assertEquals
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.threeten.bp.Clock
