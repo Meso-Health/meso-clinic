84fb2f22ef4152ef04b41b710613d59eb443b555
liangmicha <liangmicha@gmail.com>
2018-07-25 21:13:42 -0700

    Update styles in log-in screen (#354)
    * Update styling of log-in page
    * Update copy for wrong username / password combination
    * Remove progress bar and instead change button text to logging in

--- app/src/ethiopia/java/org/watsi/uhp/activities/AuthenticationActivity.kt
+++ app/src/ethiopia/java/org/watsi/uhp/activities/AuthenticationActivity.kt
@@ -3,20 +3,15 @@ package org.watsi.uhp.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import dagger.android.support.DaggerAppCompatActivity
 import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_authentication.error_text
 import kotlinx.android.synthetic.main.activity_authentication.login_button
 import kotlinx.android.synthetic.main.activity_authentication.login_password
 import kotlinx.android.synthetic.main.activity_authentication.login_username
-import me.philio.pinentry.PinEntryView
 import org.watsi.device.managers.Logger
 import org.watsi.device.managers.SessionManager
 import org.watsi.uhp.BaseApplication
@@ -43,30 +38,29 @@ class AuthenticationActivity : DaggerAppCompatActivity() {
         keyboardManager.showKeyboard(login_username)
         ActivityHelper.setupBannerIfInTrainingMode(this)
 
-        val watcher = LoginTextWatcher(login_username, login_password, login_button)
-
-        login_username.addTextChangedListener(watcher)
-        login_password.addTextChangedListener(watcher)
-
         login_button.setOnClickListener {
             login_username.clearFocus()
             login_password.clearFocus()
             keyboardManager.hideKeyboard(it)
 
+            login_button.text = getString(R.string.logging_in)
+            login_button.isEnabled = false
             sessionManager.login(login_username.text.toString(), login_password.text.toString())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe({
                         navigateToClinicActivity()
                     }, {
                         if (it is HttpException && it.code() == 401) {
-                            error_text.text = getString(R.string.login_wrong_password_message)
+                            error_text.error = getString(R.string.login_wrong_username_or_password_message)
                         } else if (it is IOException && it.message.orEmpty().contains("unexpected end of stream")) {
-                            error_text.text = getString(R.string.login_offline_error)
+                            error_text.error = getString(R.string.login_offline_error)
                         } else {
-                            error_text.text = getString(R.string.login_generic_failure_message)
+                            error_text.error = getString(R.string.login_generic_failure_message)
                             logger.warning(it)
                         }
                         error_text.visibility = View.VISIBLE
+                        login_button.text = getString(R.string.login_button_text)
+                        login_button.isEnabled = true
                     })
         }
     }
@@ -74,25 +68,6 @@ class AuthenticationActivity : DaggerAppCompatActivity() {
     override fun attachBaseContext(base: Context) {
         localeManager = (base.applicationContext as BaseApplication).localeManager
         super.attachBaseContext(localeManager.createLocalizedContext(base))
-    }
-
-    inner class LoginTextWatcher constructor(private val usernameEdit: EditText,
-                                             private val passwordEdit: PinEntryView,
-                                             private val loginButton: Button) : TextWatcher {
-
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-            // no-op
-        }
-
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-            // no-op
-        }
-
-        override fun afterTextChanged(s: Editable) {
-            error_text.visibility = View.GONE
-            val pin = passwordEdit.text.toString()
-            loginButton.isEnabled = !(usernameEdit.text.toString().isEmpty() || pin.length < 6)
-        }
     }
 
     private fun navigateToClinicActivity() {
--- app/src/uganda/java/org/watsi/uhp/activities/AuthenticationActivity.kt
+++ app/src/uganda/java/org/watsi/uhp/activities/AuthenticationActivity.kt
@@ -2,28 +2,27 @@ package org.watsi.uhp.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import dagger.android.support.DaggerAppCompatActivity
 import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_authentication.error_text
 import kotlinx.android.synthetic.main.activity_authentication.login_button
 import kotlinx.android.synthetic.main.activity_authentication.login_password
 import kotlinx.android.synthetic.main.activity_authentication.login_username
-import me.philio.pinentry.PinEntryView
+import org.watsi.device.managers.Logger
 import org.watsi.device.managers.SessionManager
 import org.watsi.uhp.R
 import org.watsi.uhp.helpers.ActivityHelper
 import org.watsi.uhp.managers.KeyboardManager
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
 class AuthenticationActivity : DaggerAppCompatActivity() {
 
     @Inject lateinit var sessionManager: SessionManager
     @Inject lateinit var keyboardManager: KeyboardManager
+    @Inject lateinit var logger: Logger
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,43 +32,30 @@ class AuthenticationActivity : DaggerAppCompatActivity() {
         keyboardManager.showKeyboard(login_username)
         ActivityHelper.setupBannerIfInTrainingMode(this)
 
-        val watcher = LoginTextWatcher(login_username, login_password, login_button)
-
-        login_username.addTextChangedListener(watcher)
-        login_password.addTextChangedListener(watcher)
-
         login_button.setOnClickListener {
             login_username.clearFocus()
             login_password.clearFocus()
             keyboardManager.hideKeyboard(it)
 
+            login_button.text = getString(R.string.logging_in)
+            login_button.isEnabled = false
             sessionManager.login(login_username.text.toString(), login_password.text.toString())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe({
-                navigateToClinicActivity()
-            }, {
-                error_text.text = it.localizedMessage
-                error_text.visibility = View.VISIBLE
-            })
-        }
-    }
-
-    inner class LoginTextWatcher constructor(private val usernameEdit: EditText,
-                                             private val passwordEdit: PinEntryView,
-                                             private val loginButton: Button) : TextWatcher {
-
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-            // no-op
-        }
-
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-            // no-op
-        }
-
-        override fun afterTextChanged(s: Editable) {
-            error_text.visibility = View.GONE
-            val pin = passwordEdit.text.toString()
-            loginButton.isEnabled = !(usernameEdit.text.toString().isEmpty() || pin.length < 6)
+                        navigateToClinicActivity()
+                    }, {
+                        if (it is HttpException && it.code() == 401) {
+                            error_text.error = getString(R.string.login_wrong_username_or_password_message)
+                        } else if (it is IOException && it.message.orEmpty().contains("unexpected end of stream")) {
+                            error_text.error = getString(R.string.login_offline_error)
+                        } else {
+                            error_text.error = getString(R.string.login_generic_failure_message)
+                            logger.warning(it)
+                        }
+                        error_text.visibility = View.VISIBLE
+                        login_button.text = getString(R.string.login_button_text)
+                        login_button.isEnabled = true
+                    })
         }
     }
 
