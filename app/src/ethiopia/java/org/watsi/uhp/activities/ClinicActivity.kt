de3a67b16bed398c14342c9a9c87f42c1abfc4d9
Jeff Beorse <Jeff@Beorse.net>
2018-08-09 10:22:10 -0700

    Seperate AndroidManifest into flavors (#368)

--- app/src/ethiopia/java/org/watsi/uhp/activities/ClinicActivity.kt
+++ app/src/ethiopia/java/org/watsi/uhp/activities/ClinicActivity.kt
@@ -32,7 +32,7 @@ class ClinicActivity : DaggerAppCompatActivity() {
     companion object {
         private val FETCH_SERVICE_JOB_ID = 0
         private val SYNC_DATA_SERVICE_JOB_ID = 1
-        val requiredPermissions = arrayOf(Manifest.permission.CAMERA, Manifest.permission.INTERNET)
+        val requiredPermissions = arrayOf(Manifest.permission.INTERNET)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
