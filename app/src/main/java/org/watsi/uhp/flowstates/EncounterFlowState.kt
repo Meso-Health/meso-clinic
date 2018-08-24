ef464361af1242ffc20f209172c7ae9c086faf51
katrinacrisostomo <katrina@watsi.org>
2018-08-24 08:16:04 -0700

    WIP
    [#]

--- app/src/main/java/org/watsi/uhp/flowstates/EncounterFlowState.kt
+++ app/src/main/java/org/watsi/uhp/flowstates/EncounterFlowState.kt
@@ -36,6 +36,11 @@ data class EncounterFlowState(var encounter: Encounter,
         return EncounterWithItemsAndForms(encounter, encounterItems, clearEncounterFormThumbnails(), diagnoses)
     }
 
+    // passed in a member because needs Member not Member?
+    fun toEncounterWithExtras(member: Member): EncounterWithExtras {
+        return EncounterWithExtras(encounter, member, encounterItems, diagnoses, encounterForms)
+    }
+
     fun getEncounterItemsOfType(billableType: Billable.Type): List<EncounterItemWithBillable> {
         return encounterItems.filter { it.billable.type == billableType }
     }
