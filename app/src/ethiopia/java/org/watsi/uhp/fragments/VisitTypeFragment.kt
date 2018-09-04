a501bc6565e506d460633627c28c13de737c7dcd
katrinacrisostomo <katrina@watsi.org>
2018-09-04 11:51:59 -0400

    Nit
    [no tracker]

--- app/src/ethiopia/java/org/watsi/uhp/fragments/VisitTypeFragment.kt
+++ app/src/ethiopia/java/org/watsi/uhp/fragments/VisitTypeFragment.kt
@@ -33,7 +33,7 @@ class VisitTypeFragment : DaggerFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        encounterFlowState = arguments.getSerializable(DiagnosisFragment.PARAM_ENCOUNTER) as EncounterFlowState
+        encounterFlowState = arguments.getSerializable(PARAM_ENCOUNTER) as EncounterFlowState
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
