3d22a1791613a70a218002af387044baaab68ef7
katrinacrisostomo <katrina@watsi.org>
2018-09-05 14:26:52 -0400

    Add find by id query to price schedule dao
    [#159910857]

--- device/src/main/java/org/watsi/device/db/daos/PriceScheduleDao.kt
+++ device/src/main/java/org/watsi/device/db/daos/PriceScheduleDao.kt
@@ -3,8 +3,10 @@ package org.watsi.device.db.daos
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.Query
+import io.reactivex.Maybe
 import io.reactivex.Single
 import org.watsi.device.db.models.PriceScheduleModel
+import java.util.UUID
 
 @Dao
 interface PriceScheduleDao {
@@ -13,4 +15,7 @@ interface PriceScheduleDao {
 
     @Query("SELECT * FROM price_schedules")
     fun all(): Single<List<PriceScheduleModel>>
+
+    @Query("SELECT * FROM price_schedules WHERE id = :id LIMIT 1")
+    fun find(id: UUID): Maybe<PriceScheduleModel>
 }
