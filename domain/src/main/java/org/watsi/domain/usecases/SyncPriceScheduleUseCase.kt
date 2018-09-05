cf66e895130af9be755bc0dba6c150127b16cb77
katrinacrisostomo <katrina@watsi.org>
2018-09-05 01:35:33 -0400

    Start SyncPriceScheduleUseCase
    [#159910857]

---
+++ domain/src/main/java/org/watsi/domain/usecases/SyncPriceScheduleUseCase.kt
@@ -0,0 +1,21 @@
+package org.watsi.domain.usecases
+
+import io.reactivex.Completable
+import org.watsi.domain.entities.Delta
+import org.watsi.domain.repositories.DeltaRepository
+
+class SyncPriceScheduleUseCase(
+        private val priceScheduleRepository: PriceScheduleRepository,
+        private val deltaRepository: DeltaRepository
+) {
+    fun execute(): Completable {
+        return Completable.fromAction {
+            val unsyncedPriceScheduleDeltas = deltaRepository.unsynced(Delta.ModelName.PRICE_SCHEDULE).blockingGet()
+
+            unsyncedPriceScheduleDeltas.map { priceScheduleDelta ->
+                priceScheduleRepository.sync(priceScheduleDelta).blockingAwait()
+                deltaRepository.markAsSynced(listOf(priceScheduleDelta)).blockingAwait()
+            }
+        }
+    }
+}
