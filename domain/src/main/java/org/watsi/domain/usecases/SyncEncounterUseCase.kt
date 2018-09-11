88b34e7b89d31810f0c5b696c2516aad4874f1db
katrinacrisostomo <katrina@watsi.org>
2018-09-11 09:00:35 -0400

    Add check for unsynced price schedules to encounter sync
    [#159910857]

--- domain/src/main/java/org/watsi/domain/usecases/SyncEncounterUseCase.kt
+++ domain/src/main/java/org/watsi/domain/usecases/SyncEncounterUseCase.kt
@@ -19,14 +19,17 @@ class SyncEncounterUseCase(
                 Delta.ModelName.BILLABLE, Delta.Action.ADD).blockingGet()
             val unsyncedMemberIds = deltaRepository.unsyncedModelIds(
                 Delta.ModelName.MEMBER, Delta.Action.ADD).blockingGet()
+            val unsyncedPriceScheduleIds = deltaRepository.unsyncedModelIds(
+                Delta.ModelName.PRICE_SCHEDULE, Delta.Action.ADD).blockingGet()
 
             unsyncedEncounterDeltas.map { encounterDelta ->
                 val encounterWithItems = encounterRepository.find(encounterDelta.modelId).blockingGet()
                 val hasUnsyncedIdEvent = unsyncedIdEventIds.contains(encounterWithItems.encounter.identificationEventId)
                 val hasUnsyncedBillable = encounterWithItems.encounterItems.any { unsyncedBillableIds.contains(it.billableId) }
                 val hasUnsyncedMember = unsyncedMemberIds.contains(encounterWithItems.encounter.memberId)
+                val hasUnsyncedPriceSchedule = encounterWithItems.encounterItems.any { unsyncedPriceScheduleIds.contains(it.priceScheduleId) }
 
-                if (!hasUnsyncedIdEvent && !hasUnsyncedBillable && !hasUnsyncedMember) {
+                if (!hasUnsyncedIdEvent && !hasUnsyncedBillable && !hasUnsyncedMember && !hasUnsyncedPriceSchedule) {
                     encounterRepository.sync(encounterDelta).blockingAwait()
                     deltaRepository.markAsSynced(listOf(encounterDelta)).blockingAwait()
                 }
