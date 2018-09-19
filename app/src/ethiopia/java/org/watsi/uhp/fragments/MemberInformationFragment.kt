ef173c5b7ce63a4d8092b3a89750307a782866f9
katrinacrisostomo <katrina@watsi.org>
2018-09-19 15:09:30 -0400

    Update comment
    [#160574003]

--- app/src/ethiopia/java/org/watsi/uhp/fragments/MemberInformationFragment.kt
+++ app/src/ethiopia/java/org/watsi/uhp/fragments/MemberInformationFragment.kt
@@ -80,7 +80,7 @@ class MemberInformationFragment : DaggerFragment(), NavigationManager.HandleOnBa
 
         // for case when it's coming from new claim screen (new claim flow):
         arguments.getString(PARAM_MEMBERSHIP_NUMBER)?.let { membershipNumber ->
-            // note: passing in Instant.now() for preparedAt on the encounter here for now because it is non-nullable
+            // note: passing in Instant.now() for preparedAt on the encounter here because it is non-nullable
             // but preparedAt should be updated to the time at save at the end of the flow
             val encounter = Encounter(encounterId, memberId, null, Instant.now(clock), Instant.now(clock))
             encounterFlowState = EncounterFlowState(encounter, emptyList(), emptyList(), emptyList())
