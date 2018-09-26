477213131681da5a9d1b7282883c6143a4ae71ae
Byron Zhang <byron.yi.zhang@gmail.com>
2018-09-26 12:41:19 -0700

    Make preparedAt nullable.
    This allows us to programatically distinguish between to-be-prepared and to-be-submitted claims (the former have preparedAt = null). The alternative would be to have to pass a PARAM_ENCOUNTER_ACTION through every fragment in the encounter so that the last fragment can pass it to ReceiptFragment.
    
    [#160573313]

--- app/src/ethiopia/java/org/watsi/uhp/fragments/MemberInformationFragment.kt
+++ app/src/ethiopia/java/org/watsi/uhp/fragments/MemberInformationFragment.kt
@@ -79,9 +79,9 @@ class MemberInformationFragment : DaggerFragment(), NavigationManager.HandleOnBa
 
         // for case when it's coming from new claim screen (new claim flow):
         arguments.getString(PARAM_MEMBERSHIP_NUMBER)?.let { membershipNumber ->
-            // note: passing in Instant.now() for preparedAt on the encounter here because it is non-nullable
-            // but preparedAt should be updated to the time at save at the end of the flow
-            val encounter = Encounter(encounterId, memberId, null, Instant.now(clock), Instant.now(clock))
+            // note: passing in Instant.now() for occurredAt on the encounter here because it is non-nullable
+            // but occurredAt should be updated to the time at save at the end of the flow
+            val encounter = Encounter(encounterId, memberId, null, Instant.now(clock))
             encounterFlowState = EncounterFlowState(encounter, emptyList(), emptyList(), emptyList())
 
             observable = viewModel.getObservable(null, membershipNumber)
