bea189aa62be978a8d3e3c520a25073fdce07e54
Byron Zhang <byron.yi.zhang@gmail.com>
2018-12-07 18:36:47 -0800

    Show member list on household search result screen (and other tweaks): * remove household metadata header since we're not implementing this for now * add padding above and below member list
    [#162227322]

--- app/src/ethiopia/java/org/watsi/uhp/fragments/HouseholdFragment.kt
+++ app/src/ethiopia/java/org/watsi/uhp/fragments/HouseholdFragment.kt
@@ -8,12 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.ethiopia.fragment_household.member_list
 import org.threeten.bp.Clock
 import org.watsi.device.managers.Logger
 import org.watsi.domain.relations.MemberWithIdEventAndThumbnailPhoto
 import org.watsi.uhp.R
 import org.watsi.uhp.activities.ClinicActivity
 import org.watsi.uhp.adapters.MemberAdapter
+import org.watsi.uhp.helpers.RecyclerViewHelper
 import org.watsi.uhp.managers.NavigationManager
 import org.watsi.uhp.viewmodels.HouseholdViewModel
 import java.util.UUID
@@ -66,5 +68,9 @@ class HouseholdFragment : DaggerFragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         (activity as ClinicActivity).setToolbar(context.getString(R.string.household_fragment_label), R.drawable.ic_arrow_back_white_24dp)
         return inflater?.inflate(R.layout.fragment_household, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        RecyclerViewHelper.setRecyclerView(member_list, memberAdapter, context, false)
     }
 }
