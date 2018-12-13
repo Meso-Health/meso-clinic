00aa0359bd2693d199718e4056cffe44b1aec4da
Peter Kutrumbos <peter.kutrumbos@gmail.com>
2018-12-12 18:01:54 -0800

    Add member name to ClaimListItem view (#462)

--- app/src/ethiopia/java/org/watsi/uhp/views/ClaimListItem.kt
+++ app/src/ethiopia/java/org/watsi/uhp/views/ClaimListItem.kt
@@ -7,6 +7,7 @@ import kotlinx.android.synthetic.ethiopia.item_claim_list.view.claim_cbhi
 import kotlinx.android.synthetic.ethiopia.item_claim_list.view.claim_id
 import kotlinx.android.synthetic.ethiopia.item_claim_list.view.claim_price
 import kotlinx.android.synthetic.ethiopia.item_claim_list.view.medical_record_number
+import kotlinx.android.synthetic.ethiopia.item_claim_list.view.member_name
 import org.watsi.domain.relations.EncounterWithExtras
 import org.watsi.uhp.utils.CurrencyUtil
 
@@ -19,6 +20,7 @@ class ClaimListItem @JvmOverloads constructor(
         onClaimSelected: (encounterRelation: EncounterWithExtras) -> Unit
     ) {
         medical_record_number.text = encounterRelation.member.medicalRecordNumber
+        member_name.text = encounterRelation.member.name
         claim_cbhi.text = encounterRelation.member.membershipNumber
         claim_id.text = encounterRelation.encounter.shortenedClaimId()
         claim_price.text = CurrencyUtil.formatMoney(encounterRelation.price())
