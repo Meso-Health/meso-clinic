fa7c9f211ffd724d193472cb97a35fa560079532
Michael Liang <liangmicha@gmail.com>
2019-02-15 10:11:39 -0800

    Update gradle file in s3 that's being pulled
    The two files are different because they have different rollbar api keys

--- deploy_scripts/downloadSecrets.sh
+++ deploy_scripts/downloadSecrets.sh
@@ -2,4 +2,4 @@ sudo apt install python-pip python-dev
 sudo pip install awscli
 aws s3 cp s3://uhp-android-app/release-key.jks app/release-key.jks
 aws s3 cp s3://uhp-android-app/google-play-key.json app/google-play-key.json
-aws s3 cp s3://uhp-android-app/variables.gradle variables.gradle
+aws s3 cp s3://uhp-android-app/claims_submission.variables.gradle variables.gradle
