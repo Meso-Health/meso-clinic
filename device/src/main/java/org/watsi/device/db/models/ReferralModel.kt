52b92e57f26b0d2aa1b5fc913a418f7baab9205a
kat <katcri@gmail.com>
2019-02-27 12:42:24 -0500

    Address code review
    [#164173197]

--- device/src/main/java/org/watsi/device/db/models/ReferralModel.kt
+++ device/src/main/java/org/watsi/device/db/models/ReferralModel.kt
@@ -1,10 +1,19 @@
 package org.watsi.device.db.models
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
 import android.arch.persistence.room.PrimaryKey
 import java.util.UUID
 
-@Entity(tableName = "referrals")
+@Entity(tableName = "referrals",
+        foreignKeys = [
+            ForeignKey(
+                entity = EncounterModel::class,
+                parentColumns = ["id"],
+                childColumns = ["encounterId"]
+            )
+        ]
+)
 data class ReferralModel(
         @PrimaryKey val id: UUID,
         val referredToFacility: String,
