e37690588733c54f2bad367080e3f8885094543e
Byronium <byron.yi.zhang@gmail.com>
2019-05-01 20:03:49 -0700

    Improve error reporting: * catch all frequent non-actionable network and server errors and handle as warning instead of error * handle both chained and non-chained versions of the errors

--- app/src/main/java/org/watsi/uhp/helpers/NetworkErrorHelper.kt
+++ app/src/main/java/org/watsi/uhp/helpers/NetworkErrorHelper.kt
@@ -11,11 +11,38 @@ object NetworkErrorHelper {
         return e is RuntimeException && e.message.orEmpty().contains("Unable to resolve host")
     }
 
+    /**
+     * Intended errors caught:
+     * - java.io.IOException: unexpected end of stream on Connection <connection>
+     * - java.net.ConnectException: Failed to connect to <server name>
+     * - java.net.NoRouteToHostException: No route to host
+     * - all of the above chained / rethrown as RuntimeExceptions
+     */
     fun isServerOfflineError(e: Throwable): Boolean {
-        return e is RuntimeException && e.message.orEmpty().contains("unexpected end of stream")
+        val message = e.message.orEmpty()
+        return message.contains("unexpected end of stream") ||
+                message.contains("Failed to connect to") ||
+                message.contains("No route to host")
     }
 
+    /**
+     * Intended errors caught:
+     * - java.net.SocketException: Network is unreachable
+     * - java.net.SocketException: Connection timed out
+     * - java.net.SocketException: Software caused connection abort
+     * - java.net.SocketTimeoutException: timeout
+     * - java.net.SocketTimeoutException: connect timed out
+     * - java.net.SocketTimeoutException: SSL handshake timed out
+     * - javax.net.ssl.SSLException: Write error: ssl=<hex>: I/O error during system call, Software caused connection abort
+     * - javax.net.ssl.SSLException: Read error: ssl=<hex>: I/O error during system call, Software caused connection abort
+     * - javax.net.ssl.SSLHandshakeException: SSL handshake aborted: ssl=<hex>: I/O error during system call, Connection timed out
+     * - all of the above chained / rethrown as RuntimeExceptions
+     */
     fun isPoorConnectivityError(e: Throwable): Boolean {
-        return e is RuntimeException && e.message.orEmpty().contains("timeout")
+        val message = e.message.orEmpty()
+        return message.contains("Network is unreachable") ||
+                message.contains("timeout") ||
+                message.contains("timed out") ||
+                message.contains("connection abort")
     }
 }
