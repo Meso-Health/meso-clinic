8090372bfc96039172b22b280a589707e08dc6b6
g e link <g.link@watsi.org>
2019-08-16 14:38:53 -0500

    update checks

--- app/src/ethiopia/java/org/watsi/uhp/fragments/ReceiptFragment.kt
+++ app/src/ethiopia/java/org/watsi/uhp/fragments/ReceiptFragment.kt
@@ -363,8 +363,7 @@ class ReceiptFragment : DaggerFragment(), NavigationManager.HandleOnBack {
             return
         }
 
-        if (encounterFlowState.encounter.adjudicationState == Encounter.AdjudicationState.RETURNED
-                && (encounterFlowState.newProviderComment == null || encounterFlowState.newProviderComment?.length == 0)) {
+        if (encounterAction == EncounterAction.RESUBMIT && encounterFlowState.newProviderComment.isNullOrBlank()) {
             SnackbarHelper.showError(finish_button, context, getString(R.string.empty_comment_warning))
             return
         }
