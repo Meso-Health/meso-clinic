e3d333391d818a261ef1e9f4fcc3dfba6e15d4dd
liangmicha <liangmicha@gmail.com>
2019-10-11 10:52:41 -0700

    Auto-deploy to uganda sandbox when merge to master (#613)
    When merge master the following will happen:
    - deploy to uganda sandbox
    - deploy to ethiopia staging

---
+++ deploy_scripts/downloadUgandaSecrets.sh
@@ -0,0 +1,5 @@
+sudo apt install python-pip python-dev
+sudo pip install awscli
+aws s3 cp s3://uhp-android-app/release-key.jks app/release-key.jks
+aws s3 cp s3://uhp-android-app/google-play-key.json app/google-play-key.json
+aws s3 cp s3://uhp-android-app/uganda.claims_submission.variables.gradle variables.gradle
