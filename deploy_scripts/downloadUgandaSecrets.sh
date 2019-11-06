e13b557bea42f11130dc00a08cb887b555754042
liangmicha <liangmicha@gmail.com>
2019-11-06 13:53:06 -0800

    Updated to the meso-secrets bucket (#633)
    Created a new bucket on S3 called `meso-secrets` to replace `uhp-android-app` bucket.
    
    This bucket contains the keys / secrets needed to deploy to play store for **both** clinic and enrollment apps. It felt a little weird naming the bucket `uhp-android-app` (which refers to clinic app) when secrets for both apps live there. Also wanted to be super clear this bucket holds secrets and not other things like member photos.
    
    Also prefixes each `variables.gradle` with the app and flavor.

--- deploy_scripts/downloadUgandaSecrets.sh
+++ deploy_scripts/downloadUgandaSecrets.sh
@@ -1,5 +1,5 @@
 sudo apt install python-pip python-dev
 sudo pip install awscli
-aws s3 cp s3://uhp-android-app/release-key.jks app/release-key.jks
-aws s3 cp s3://uhp-android-app/google-play-key.json app/google-play-key.json
-aws s3 cp s3://uhp-android-app/uganda.claims_submission.variables.gradle variables.gradle
+aws s3 cp s3://meso-secrets/release-key.jks app/release-key.jks
+aws s3 cp s3://meso-secrets/google-play-key.json app/google-play-key.json
+aws s3 cp s3://meso-secrets/uganda.claims_submission.variables.gradle variables.gradle
